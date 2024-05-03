6433d824175a5abb01bf80b7e728116ccce167c9
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-03 10:32:32 +0200

    console log , error , warn , table , prompt

---
+++ js01_Intro_Variables/app.js
@@ -0,0 +1,20 @@
+// console.log("Liebe Grüße");
+
+console.log("burasi console alanidir");
+
+console.error("bu bir hata mesajidir");
+
+console.warn("Warning");
+
+// console.clear();
+
+console.table([
+  { name: "umut", age: 30 },
+  { name: "umut", age: 25 },
+]);
+
+// alert("Burasi Alert");
+
+//Prompt Kullanimi
+name1 = prompt("Enter your name");
+console.log(name1);
