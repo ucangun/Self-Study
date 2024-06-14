aae61773051c5f3029dbb9a095263d917cffe650
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-14 09:52:54 +0200

    gelir kismi add

--- js25_Wallet_App/app.js
+++ js25_Wallet_App/app.js
@@ -5,27 +5,27 @@ const gelirInput = document.getElementById("gelir-input");
 const ekleFormu = document.getElementById("ekle-formu");
 
 //! Hesap Tablosu
-
 const gelirinizTd = document.getElementById("geliriniz");
 const giderinizTd = document.getElementById("gideriniz");
-const totalTd = document.getElementById("total");
+const kalanTd = document.getElementById("kalan");
 
-// ! Variables
-
+//! variables
 let gelirler = 0;
 
-//* Ekle Formu
+//? Ekle Formu
 
-ekleBtn.addEventListener("submit", (e) => {
+ekleFormu.addEventListener("submit", (e) => {
   e.preventDefault();
   gelirler = gelirler + Number(gelirInput.value);
-  //gelirInput.value = "";
+  //console.log(gelirler);
+  // gelirInput.value = ''
   ekleFormu.reset();
   localStorage.setItem("gelirler", gelirler);
   gelirinizTd.textContent = gelirler;
+  gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler);
 });
 
 window.addEventListener("load", () => {
-  gelirler = Number(localStorage.getItem("gelirler"));
-  gelirinizTd.textContent = gelirler;
+  gelirler = Number(localStorage.getItem("gelirler")) || 0;
+  gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler);
 });
