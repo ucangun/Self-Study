cbe0b7032f38b49770969bf3fb3ebf4b888d6c8b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-25 10:47:43 +0200

    project with meal api

---
+++ js27_Async-Project1/meal.js
@@ -0,0 +1,39 @@
+//www.themealdb.com/
+
+let dizi = [];
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+  .then((resp) => resp.json())
+  .then((data) => {
+    dizi = data;
+    showScreen(data.meals);
+  });
+
+//! EKRANA BASTIRMA
+
+function showScreen(data) {
+  const mealsDiv = document.querySelector(".food");
+
+  mealsDiv.innerHTML = "";
+  data.forEach((a) => {
+    mealsDiv.innerHTML += `
+    <div class="col-md-3">
+    <p>${a.strMeal}</p>
+    <img width="200px" src="${a.strMealThumb}"/>
+    <div>
+    `;
+  });
+}
+
+//!arama inputuna yazdığım harfleri içeren yemekleri ekrana bastır (oninput=kullanıcı input elemanına her veri girişinde çalışmaktadır)
+
+//? e.target.value=> inputa her veri girişindeki değerleri alabilmek için kullanılır.document.querySelector("input").value=e.target.value
+
+document.querySelector("input").oninput = (e) => {
+  //console.log(e.target.value);
+
+  let veri = dizi.meals.filter((a) =>
+    a.strMeal.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  showScreen(veri);
+};
