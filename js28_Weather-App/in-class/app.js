82be08fb0a2c6163c195889989a96828437012a0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-27 11:16:37 +0200

    card control and alert message

--- js28_Weather-App/in-class/app.js
+++ js28_Weather-App/in-class/app.js
@@ -5,12 +5,14 @@
 const form = document.querySelector("form");
 const input = document.querySelector("form input.form-control");
 const cardContainer = document.getElementById("card-container");
+const alertMessage = document.getElementById("alert");
 
 //! Variables
 let apiKey = "75b251ce9d3d5c7bf9e4f1832b237076";
 let url; // api request
 let units = "metric"; // Fahrenheit - imperial
 let lang = "en"; // for Deutsch DE
+let cities = [];
 
 //! Event Listeners
 
@@ -40,37 +42,46 @@ const getWeatherData = async () => {
 
     const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`; //^ openweathermap.org
 
-    const card = document.createElement("div");
-    card.classList.add("col");
-    card.setAttribute("id", `${name}`);
+    if (!cities.includes(name)) {
+      cities.push(name);
+      const card = document.createElement("div");
+      card.classList.add("col");
+      card.setAttribute("id", `${name}`);
 
-    card.innerHTML = ` 
-<div class="card mb-4 rounded-3 shadow-sm">
-        <ul class="list-unstyled mt-2 mb-4">
-            <li class="text-end me-2"><i class="bi bi-x-circle"></i></li>
-            <h4 class="my-0 fw-normal">${name} <span ><sup><img src="https://flagsapi.com/${
-      sys.country
-    }/shiny/24.png" class="rounded-circle" alt=${
-      sys.country
-    }/> </sup></span></h4>
-            <h1 class="card-title pricing-card-title"><i class="bi bi-thermometer-half"></i> ${Math.round(
-              main.temp
-            )}<sup>°C</sup></h1>
-            <h6 class="card-title pricing-card-title">Min : ${Math.round(
-              main.temp_min
-            )}<sup>°C</sup> - Max : ${Math.round(
-      main.temp_max
-    )}<sup>°C</sup>  </h6>
-            <h6 class="card-title pricing-card-title"><img src="./assets/wi-barometer.svg" height="30px"/>${
-              main.pressure
-            } <img src="./assets/wi-humidity.svg" height="30px"/>${
-      main.humidity
-    } </h6>
-            <li><img src="${iconUrl}"/></li>
-            <li>${weather[0].description.toUpperCase()}</li>
-        </ul>
-</div>
-`;
-    cardContainer.appendChild(card);
+      card.innerHTML = ` 
+  <div class="card mb-4 rounded-3 shadow-sm">
+          <ul class="list-unstyled mt-2 mb-4">
+              <li class="text-end me-2"><i class="bi bi-x-circle"></i></li>
+              <h4 class="my-0 fw-normal">${name} <span ><sup><img src="https://flagsapi.com/${
+        sys.country
+      }/shiny/24.png" class="rounded-circle" alt=${
+        sys.country
+      }/> </sup></span></h4>
+              <h1 class="card-title pricing-card-title"><i class="bi bi-thermometer-half"></i> ${Math.round(
+                main.temp
+              )}<sup>°C</sup></h1>
+              <h6 class="card-title pricing-card-title">Min : ${Math.round(
+                main.temp_min
+              )}<sup>°C</sup> - Max : ${Math.round(
+        main.temp_max
+      )}<sup>°C</sup>  </h6>
+              <h6 class="card-title pricing-card-title"><img src="./assets/wi-barometer.svg" height="30px"/>${
+                main.pressure
+              } <img src="./assets/wi-humidity.svg" height="30px"/>${
+        main.humidity
+      } </h6>
+              <li><img src="${iconUrl}"/></li>
+              <li>${weather[0].description.toUpperCase()}</li>
+          </ul>
+  </div>
+  `;
+      cardContainer.appendChild(card);
+    } else {
+      alertMessage.classList.remove("d-none");
+      alertMessage.textContent = `You already know the weather for ${name}, Please search for another city 😉`;
+      setTimeout(() => {
+        alertMessage.classList.add("d-none");
+      }, 3000);
+    }
   } catch (error) {}
 };
