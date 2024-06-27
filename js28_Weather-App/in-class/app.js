69d585a3dc0d2e0e41b61a215245c9ccd068b7b4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-27 09:46:29 +0200

    selectors , variables , and form click add

--- js28_Weather-App/in-class/app.js
+++ js28_Weather-App/in-class/app.js
@@ -1,1 +1,20 @@
-console.log('App.js working')
+console.log("App.js working");
+
+//! Selectors
+
+const form = document.querySelector("form");
+const input = document.querySelector("form input.form-control");
+
+//! Variables
+let apiKey = "75b251ce9d3d5c7bf9e4f1832b237076";
+let url; // api request
+let units = "metric"; // Fahrenheit - imperial
+let lang = "en"; // for Deutsch DE
+
+//! Event Listeners
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&lang=${lang}&apiKey`;
+});
