5f4f29f58902798798423147ef90b68efcb94c1f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-02 09:33:08 +0200

    named export

---
+++ js30_Modules/index.js
@@ -0,0 +1,16 @@
+console.log("selamlar");
+
+import {
+  topla,
+  calismaSaati,
+  increase,
+  decrease,
+  myName,
+} from "./myModules.js";
+
+console.log(topla(3, 5));
+console.log(calismaSaati);
+
+console.log(increase(5));
+console.log(decrease(1));
+console.log(myName);
---
+++ js30_Modules/myModules.js
@@ -0,0 +1,27 @@
+console.log("myModule 'den selamlar ");
+
+//*************************************/
+//* ES-MODULES
+//*************************************/
+//? ES Modules, EcmaScript 2015 versiyonu ile gelmiştir.
+//? Günümüzde birçok browser ES modül yapısını desteklemektedir.
+//? JS kodlarının kolay ve performansli bir şekilde farklı dosyalara ayrilmasina ve gerektiginde birleştirmesine olanak saglamaktadir.
+//? Bir degisken,dizi,obje, fonksiyon, class v.b "export" deyimi ile başka dosyalarinin kullanimina açilmabilir.
+//? Kullanima acilanlar ise import deyimi ile istenilen dosyaya eklenebilir.
+
+//* named export
+export const calismaSaati = 40;
+
+export function topla(s1, s2) {
+  return s1 + s2;
+}
+
+let counter = 5;
+
+const increase = (miktar) => (counter += miktar);
+
+const decrease = (miktar) => (counter -= miktar);
+
+const myName = "Felix";
+
+export { increase, decrease, myName };
