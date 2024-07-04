54fcdfb1a218605e3ddd2d230c8b7e4937702d7d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-04 12:05:18 +0200

    "Added Card component to App.js, removed Card.jsx, and modified MyNavbar.jsx"

--- React/rc03_react-bootstrap/src/App.js
+++ React/rc03_react-bootstrap/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
 import Slider from "./components/Slider";
+import Card from "./components/MyCard";
+import data from "./data";
 
 function App() {
   return (
     <div>
       <MyNavbar />
       <Slider />
+      <Card veri={data} />
     </div>
   );
 }
--- React/rc03_react-bootstrap/src/components/MyNavbar.jsx
+++ React/rc03_react-bootstrap/src/components/MyNavbar.jsx
@@ -7,6 +7,8 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../img/logo.png";
 
 const MyNavbar = () => {
+  //!navbar scrolling   expand="lg"=yazılar, lg ekranlarda görünmeye başlar. navbarScroll=hamburger açılınca sığmayan linkler için scrollbar
+
   return (
     <Navbar expand="lg" bg="light">
       <Container fluid>
