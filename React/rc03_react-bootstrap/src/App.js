a38d9a409f2673327f9860e46e073620f8efc269
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-04 10:37:37 +0200

    "Added Slider component to App.js, updated MyNavbar component with logo and modified navigation links"

--- React/rc03_react-bootstrap/src/App.js
+++ React/rc03_react-bootstrap/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div>
       <MyNavbar />
+      <Slider />
     </div>
   );
 }
--- React/rc03_react-bootstrap/src/components/MyNavbar.jsx
+++ React/rc03_react-bootstrap/src/components/MyNavbar.jsx
@@ -4,12 +4,15 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../img/logo.png";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" bg="light">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -18,20 +21,13 @@ const MyNavbar = () => {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
+            <Nav.Link href="#action2">People</Nav.Link>
+            <NavDropdown title="Courses" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Full-Stack</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Data-Science</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">AWS-Devops</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
