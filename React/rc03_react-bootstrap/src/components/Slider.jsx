11ea96db3a96f2d422f42341b58ffb9457f55dc9
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-04 10:56:03 +0200

    "Added Carousel component and images to Slider component"

--- React/rc03_react-bootstrap/src/components/Slider.jsx
+++ React/rc03_react-bootstrap/src/components/Slider.jsx
@@ -1,5 +1,36 @@
+import Carousel from "react-bootstrap/Carousel";
+import img1 from "../img/slider01.jpg";
+import img2 from "../img/slider02.jpg";
+import img3 from "../img/slider03.jpg";
+
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={img1} alt="img" text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img2} alt="img" text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img3} alt="img" text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default Slider;
