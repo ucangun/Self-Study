426d7106e75a488f1c003c057525f6f1884268ac
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-04 10:05:38 +0200

    "Added Bootstrap CSS import and MyNavbar component to App.js"

--- React/rc03_react-bootstrap/src/App.js
+++ React/rc03_react-bootstrap/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>DE 09</h1>
+    <div>
+      <MyNavbar />
     </div>
   );
 }
