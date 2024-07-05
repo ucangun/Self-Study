e7dfb103c397103304acc4e2c4a6a5017b9fa7db
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-05 09:40:11 +0200

    "Updated App.js to import App.scss and Header component, replaced h1 tag with Header component, and created new App.scss file with imports and styles for body and h1 elements."

--- React/rc03_SASS/src/App.js
+++ React/rc03_SASS/src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>DE 09</h1>
+      <Header />
     </div>
   );
 }
