b329ef2a77c76889604254ed8644f6bb94f789bc
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-09 09:13:20 +0200

    add react project files

---
+++ React/rc05_06_Events-Hooks/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DE 09</h1>
+    </div>
+  );
+}
+
+export default App;
