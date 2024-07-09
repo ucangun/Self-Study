a722e1bfdd97cf025e8579b9b897db8d585f4806
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-09 10:17:28 +0200

    "Added comments explaining what Hooks are, their rules, and a reference link in Hooks.jsx file."

--- React/rc05_06_Events-Hooks/src/2-hooks/Hooks.jsx
+++ React/rc05_06_Events-Hooks/src/2-hooks/Hooks.jsx
@@ -1,3 +1,21 @@
+//* ==================== HOOKS ===============================
+//! Hook’lar fonksiyonel component’ler icerisinde state’leri kullanmamiza
+//! olanak saglayan ozel fonksiyonlardir.
+//! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler’in
+//! kullanimini cok azaltmistir.
+//? React’ta useState(), useEffect(), useContext() gibi bir cok built-in
+//? Hook bulunmaktadir. Ayrica custom hook tanimlamak da mumkundur.
+//* Hook Kullanim Kurallari:
+//* 1. İlk olarak import edilmeliler. import { useState } from “react”;
+//* 2. Hook’lar ust seviyede tanimlanmalidir. Yani Hook’lar bir
+//*    dongunun, kosul cumleciginin ve icice fonksiyonlarin icerisinde
+//*    kullanilmamalidir.
+//* 3. Hook’lar sadece React Fonksiyonel componentleri icerisinde cagrilmalidir.
+//*    Normal Javascript fonksiyonlari icerisinde cagrilmamalidir
+//*    (Custom hook’lar icerisinde bir hook cagrilabilir)
+//?    https://react.dev/reference/react/hooks#state-hooks
+//* =============================================================
+
 const Hooks = () => {
   return <div>Hooks</div>;
 };
