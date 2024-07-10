f0d3dc968704c634e9eb51f4073ba987fb610907
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 11:33:29 +0200

    "Added destructuring of `info` object, created `sendToDatabase` function, and updated form fields with `value` props and `onSubmit` event handler."

--- React/rc05_06_Events-Hooks/src/4-form/FormObject.jsx
+++ React/rc05_06_Events-Hooks/src/4-form/FormObject.jsx
@@ -7,18 +7,34 @@ const FormObject = () => {
     password: "",
   });
 
+  const { name, email, password } = info;
+
   const handleInfo = (e) => {
-    e.preventDefault();
     setInfo({ ...info, [e.target.id]: e.target.value });
-    console.log(e);
   };
+
+  const sendToDatabase = (e) => {
+    e.preventDefault();
+    alert(`
+      name: ${name}
+      password :${password}
+      email : ${email}
+    
+      `);
+    setInfo({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="mt-4 p-3">
       <div className="text-center">
         <h1>*********************************</h1>
         <h2>FORM EVENTS</h2>
       </div>
-      <form>
+      <form onSubmit={sendToDatabase}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name: <span className="text-danger">{} </span>
@@ -29,6 +45,7 @@ const FormObject = () => {
             id="name"
             required
             onChange={handleInfo}
+            value={name}
           />
         </div>
         <div className="mb-3">
@@ -41,6 +58,7 @@ const FormObject = () => {
             id="email"
             required
             onChange={handleInfo}
+            value={email}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
@@ -56,6 +74,7 @@ const FormObject = () => {
             id="password"
             onChange={handleInfo}
             required
+            value={password}
           />
         </div>
 
