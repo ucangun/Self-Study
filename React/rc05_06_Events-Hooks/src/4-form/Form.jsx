f9431323b8425865b8cd8189a6248e170efaf11f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 10:14:58 +0200

    "Added form component with input fields and submit button, and updated App.js to render the form"

--- React/rc05_06_Events-Hooks/src/4-form/Form.jsx
+++ React/rc05_06_Events-Hooks/src/4-form/Form.jsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [name, setName] = useState("");
   return (
     <div className="mt-4">
-      <div>
+      <div className="text-center">
         <h1>******************************************</h1>
         <h2>FORM EVENTS</h2>
       </div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name : <span className="text-danger">{name}</span>
+          </label>
+
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            aria-describedby="emailHelp"
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="remember" />
+          <label className="form-check-label" htmlFor="remember">
+            Remember
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
