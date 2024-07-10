721602988f348f3692f3684be2b755c12980b218
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 09:51:59 +0200

    "Comment out Events, Hooks, and Counter components, add Form component to App.js"

---
+++ React/rc05_06_Events-Hooks/src/4-form/Form.jsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div className="mt-4">
+      <div>
+        <h1>******************************************</h1>
+        <h2>FORM EVENTS</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
--- React/rc05_06_Events-Hooks/src/App.js
+++ React/rc05_06_Events-Hooks/src/App.js
@@ -1,15 +1,17 @@
 import Events from "./1-events-hooksIntro/Events";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./3-classComponents/Counter";
+import Form from "./4-form/Form";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Events />
-      <Hooks />
-      <Counter />
+      {/* <Events /> */}
+      {/* <Hooks /> */}
+      {/* <Counter /> */}
+      <Form />
     </div>
   );
 }
