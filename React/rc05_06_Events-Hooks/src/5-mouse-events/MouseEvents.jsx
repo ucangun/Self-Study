841737d153be9cd12637fde97af2aa3ec686d8f2
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 12:05:41 +0200

    "Added handleDoubleClick function and event handler to div element"

--- React/rc05_06_Events-Hooks/src/5-mouse-events/MouseEvents.jsx
+++ React/rc05_06_Events-Hooks/src/5-mouse-events/MouseEvents.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 const MouseEvents = () => {
   const [visible, setVisible] = useState(false);
+
+  const handleDoubleClick = (e) => {
+    e.target.classList.toggle("bg-danger");
+  };
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
@@ -15,7 +19,11 @@ const MouseEvents = () => {
       </div>
       {/* Conditional Rendering */}
       <p className="mt-2">{visible && <span>For detail ....</span>}</p>
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
         onDoubleClick
       </div>
       <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
