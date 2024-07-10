9bda57822f7fd1f7972ffbd1298e39bba7659c75
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 12:39:03 +0200

    "Added keyboard event handlers for input and textarea elements"

--- React/rc05_06_Events-Hooks/src/6-keyboard-events/Keyboard.jsx
+++ React/rc05_06_Events-Hooks/src/6-keyboard-events/Keyboard.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
 const Keyboard = () => {
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    console.log(e.key);
+    if (e.key === "Enter") {
+      alert("From submitted");
+    }
+  };
+
+  const handleCopy = (e) => {
+    alert("you not allowed to copy this text");
+    e.preventDefault();
+    // console.log("copied");
+  };
+
+  const handleCut = (e) => {
+    alert("you not allowed to cut this text");
+    e.preventDefault();
+  };
+
   return (
     <div className="container mt-4">
       <h2>Keyboard Events</h2>
 
-      <input type="text" className="form-control" />
+      <input type="text" className="form-control" onKeyDown={handleKeyDown} />
 
       <div className="mt-3">
         <p>{}</p>
@@ -18,6 +37,8 @@ const Keyboard = () => {
           id="area"
           cols="30"
           rows="10"
+          onCopy={handleCopy}
+          onCut={handleCut}
         ></textarea>
       </div>
     </div>
