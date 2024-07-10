74fd0a4870b44d64f8a46c5749deb27813c73af7
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 10:44:50 +0200

    "Added password, country, and remember fields to Form component with corresponding state variables and event handlers."

--- React/rc05_06_Events-Hooks/src/4-form/Form.jsx
+++ React/rc05_06_Events-Hooks/src/4-form/Form.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 const Form = () => {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [country, setCountry] = useState("");
+  const [remember, setRemember] = useState("");
   return (
     <div className="mt-4">
       <div className="text-center">
@@ -21,22 +24,45 @@ const Form = () => {
             aria-describedby="emailHelp"
             onChange={(e) => setName(e.target.value)}
           />
-
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
-            Password
+            Password : <span className="text-danger">{password}</span>
           </label>
 
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+        <div className="mb-3">
+          <label htmlFor="country" className="form-label">
+            Country : <span className="text-danger">{country}</span>
+          </label>
+          <select
+            id="country"
+            className="form-select"
+            onChange={(e) => setCountry(e.target.value)}
+          >
+            <option>Countries</option>
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
+        </div>
+
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="remember" />
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="remember"
+            onChange={(e) => setRemember(e.target.checked)}
+          />
           <label className="form-check-label" htmlFor="remember">
-            Remember
+            Remember :{" "}
+            <span className="text-danger">{remember.toString()}</span>
           </label>
         </div>
 
