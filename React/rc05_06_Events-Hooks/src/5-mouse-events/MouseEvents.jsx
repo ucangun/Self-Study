cd20d5f3e3ce7856b92509428da2d987b729207a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 12:19:24 +0200

    "Added comments explaining mouse events, implemented onMouseMove, onMouseOver, and onMouseEnter events, and displayed mouse coordinates"

--- React/rc05_06_Events-Hooks/src/5-mouse-events/MouseEvents.jsx
+++ React/rc05_06_Events-Hooks/src/5-mouse-events/MouseEvents.jsx
@@ -1,10 +1,39 @@
 import { useState } from "react";
 
+//* Mouseover olayı, fare işaretçisi div öğesine ve onun alt öğelerine girdiğinde tetiklenir.
+
+//* Mouseenter olayı yalnızca fare işaretçisi div öğesine girdiğinde tetiklenir.
+
+//* Onmousemove olayı, fare işaretçisi div öğesinin üzerinde her hareket ettiğinde tetiklenir.
+
+//? pageX/Ykoordinatlar, oluşturulan sayfanın tamamının sol üst köşesine göredir (kaydırılarak gizlenen parçalar dahil),
+
+//? clientX/YKoordinatlar sayfanın görünen kısmının sol üst köşesine göredir ve tarayıcı penceresinden "görünür" .
+
+//? screenX/Yfiziksel ekrana göredir.
+
 const MouseEvents = () => {
   const [visible, setVisible] = useState(false);
+  const [coordX, setCoordX] = useState("");
+  const [coordY, setCoordY] = useState("");
 
   const handleDoubleClick = (e) => {
     e.target.classList.toggle("bg-danger");
+  };
+
+  const handleMouseMove = (e) => {
+    //? Mutlak Koordinatlar
+
+    // console.log("X:", e.pageX);
+    // console.log("Y:", e.pageY);
+    // console.log("X:", e.clientX);
+    // console.log("Y:", e.clientY);
+
+    //? Göreceli Koordinatlar
+    console.log("X:", e.nativeEvent.offsetX);
+    console.log("Y:", e.nativeEvent.offsetY);
+    setCoordX(e.nativeEvent.offsetX);
+    setCoordY(e.nativeEvent.offsetY);
   };
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
@@ -26,16 +55,28 @@ const MouseEvents = () => {
       >
         onDoubleClick
       </div>
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseMove={handleMouseMove}
+      >
         <span>onMouseMove</span>
       </div>
-      <div id="todo-4" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-4"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseOver={handleMouseMove}
+      >
         <span>onMouseOver</span>
       </div>
-      <div id="todo-5" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-5"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseEnter={handleMouseMove}
+      >
         <span>onMouseEnter</span>
       </div>
-      X: {} and Y:{}
+      X: {coordX} and Y:{coordY}
     </div>
   );
 };
