13160c82e3970ad8d599b2926b6257a4dffb6de8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-12 09:30:37 +0200

    "Added Bootstrap CSS and components, replaced App.js content with Container and Header component, and implemented Header component with NBA logo and title"

--- React/rc07_NBA_Legends/src/App.js
+++ React/rc07_NBA_Legends/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>DE 09</h1>
-    </div>
+    <Container className="text-center">
+      <Header />
+    </Container>
   );
 }
 
--- React/rc07_NBA_Legends/src/components/Header.jsx
+++ React/rc07_NBA_Legends/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { Container, Image } from "react-bootstrap";
+import logo from "../assets/nba-logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container className="text-center">
+      <Image width="200px" src={logo} alt="nba logo" />
+      <h1 className="fw-bold font-monospace my-2 display-5">NBA Legends</h1>
+    </Container>
+  );
 };
 
 export default Header;
