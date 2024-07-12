0b2b574d86946e590f96df5f74ffda0c8b92fa88
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-12 10:32:48 +0200

    "Added import statement for react-bootstrap Card component and implemented PlayerCard component with Card, Card.Img, and Card.Footer elements."

--- React/rc07_NBA_Legends/src/components/PlayerCard.jsx
+++ React/rc07_NBA_Legends/src/components/PlayerCard.jsx
@@ -1,5 +1,14 @@
+import { Card } from "react-bootstrap";
+
 const PlayerCard = ({ name, img, statistics }) => {
-  return <div>PlayerCard</div>;
+  return (
+    <Card className="rounded-2 m-auto playerCard" role="button">
+      <Card.Img className="playerImg" variant="top" src={img} />
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
+    </Card>
+  );
 };
 
 export default PlayerCard;
