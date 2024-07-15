b00360f7a8e00e1c4a18cddae9389fb59047882a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-15 10:39:00 +0200

    "Added deleteTodo and checkTodo functions to TodoList component, and attached them to MdDeleteForever and FaCheckCircle icons respectively."

--- React/rc08_Todo_App/src/components/TodoList.jsx
+++ React/rc08_Todo_App/src/components/TodoList.jsx
@@ -2,17 +2,31 @@ import { MdDeleteForever } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
 
 const TodoList = ({ doing, setDoing }) => {
+  const deleteTodo = (id) => {
+    setDoing(doing.filter((item) => item.id !== id));
+  };
+
+  const checkTodo = (id) => {
+    setDoing(
+      doing.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+
   return (
     <div>
       {doing.map((x) => (
-        <div key={x.id}>
+        <div className={x.isDone ? "done" : "gorev"} key={x.id}>
           <h3>
             {x.text}
             <div>
               <FaCheckCircle
+                onClick={() => checkTodo(x.id)}
                 style={{ color: "green", fontSize: "20px", cursor: "pointer" }}
               />
               <MdDeleteForever
+                onClick={() => deleteTodo(x.id)}
                 style={{ color: "red", fontSize: "25px", cursor: "pointer" }}
               />
             </div>
