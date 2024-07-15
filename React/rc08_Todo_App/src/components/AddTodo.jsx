3f4e0ffedfd03c11c64529b523e36ded846439dc
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-15 11:17:43 +0200

    "Added state to toggle task bar and conditional rendering of form elements"

--- React/rc08_Todo_App/src/components/AddTodo.jsx
+++ React/rc08_Todo_App/src/components/AddTodo.jsx
@@ -1,25 +1,34 @@
+import { useState } from "react";
+
 const Todo = ({ doing, setDoing }) => {
+  const [button, setButton] = useState(true);
   return (
     <div>
       <header>
         <h1>TO DO APP</h1>
-        <button className="btn" style={{ backgroundColor: "green" }}>
-          CLOSE ADD TASK BAR
+        <button
+          className="btn"
+          style={{ backgroundColor: button ? "green" : "purple" }}
+          onClick={() => setButton(!button)}
+        >
+          {button ? "CLOSE" : "SHOW"} TASK BAR
         </button>
       </header>
 
-      <form>
-        <div className="formControl">
-          <label htmlFor="text">Task</label>
-          <input type="text" id="text" />
-        </div>
-        <div className="formControl">
-          <label htmlFor="date">Day & Time</label>
-          <input type="datetime-local" id="date" />
-        </div>
+      {button && (
+        <form>
+          <div className="formControl">
+            <label htmlFor="text">Task</label>
+            <input type="text" id="text" />
+          </div>
+          <div className="formControl">
+            <label htmlFor="date">Day & Time</label>
+            <input type="datetime-local" id="date" />
+          </div>
 
-        <button className="btn btn-submit">SUBMIT</button>
-      </form>
+          <button className="btn btn-submit">SUBMIT</button>
+        </form>
+      )}
     </div>
   );
 };
