496183e47e8f00600ecfa7049f6e4669d30545d0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-15 12:09:27 +0200

    "Added state variables for text and day, implemented handleSubmit function, and updated form inputs and button to use state and handleSubmit"

--- React/rc08_Todo_App/src/components/AddTodo.jsx
+++ React/rc08_Todo_App/src/components/AddTodo.jsx
@@ -2,6 +2,25 @@ import { useState } from "react";
 
 const Todo = ({ doing, setDoing }) => {
   const [button, setButton] = useState(true);
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) return;
+    const newDoing = {
+      id: crypto.randomUUID(),
+      text: text,
+      day: day,
+      isDone: false,
+    };
+
+    setDoing([...doing, newDoing]);
+    setText("");
+    setDay("");
+  };
+
   return (
     <div>
       <header>
@@ -16,17 +35,29 @@ const Todo = ({ doing, setDoing }) => {
       </header>
 
       {button && (
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="formControl">
             <label htmlFor="text">Task</label>
-            <input type="text" id="text" />
+            <input
+              type="text"
+              id="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
           </div>
           <div className="formControl">
             <label htmlFor="date">Day & Time</label>
-            <input type="datetime-local" id="date" />
+            <input
+              type="datetime-local"
+              id="date"
+              value={day}
+              onChange={(e) => setDay(e.target.value)}
+            />
           </div>
 
-          <button className="btn btn-submit">SUBMIT</button>
+          <button className="btn btn-submit" type="submit">
+            SUBMIT
+          </button>
         </form>
       )}
     </div>
