018802ea3237e043b9fd0c9c862feaf2a90ba4ae
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-18 09:55:11 +0200

    "Added useState hook and conditional rendering to App component, also imported bootstrap CSS and updated JSX structure."

--- React/rc10_LifeCycle/src/App.js
+++ React/rc10_LifeCycle/src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import LifeCycleMethods from "./LifeCycleMethods";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
-    <div>
-      <LifeCycleMethods />
+    <div className="text-center container mt-4">
+      <button onClick={() => setShow(!show)} className="btn btn-primary">
+        SHOW
+      </button>
+      {show && <LifeCycleMethods />}
     </div>
   );
 }
