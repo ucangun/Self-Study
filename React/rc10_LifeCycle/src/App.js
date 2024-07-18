b9a96984bca3c2acf66b6b6ab70987aadaf450b0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-18 09:41:22 +0200

    add in-class files & LifeCyleMethods

---
+++ React/rc10_LifeCycle/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LifeCycleMethods from "./LifeCycleMethods";
+
+function App() {
+  return (
+    <div>
+      <LifeCycleMethods />
+    </div>
+  );
+}
+
+export default App;
