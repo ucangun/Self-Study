b5edb83163991977df40e11b316953ccdcdbc935
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-23 09:27:15 +0200

    "Added state, effect, and API call to Teacher component to fetch and display user data"

--- React/rc12_13_ReactRouter/src/pages/Teacher.jsx
+++ React/rc12_13_ReactRouter/src/pages/Teacher.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Teacher = () => {
-  return <div>Teacher</div>;
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setPeople(res.data));
+  }, []);
+
+  return (
+    <div className="container text-center mt-4">
+      <div className="row">
+        {people.map(({ id, name, username, phone }) => (
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={id}>
+            {/* https://www.dicebear.com/styles/avataaars/ */}
+
+            <img
+              src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${name}`}
+              alt=""
+            />
+
+            <h5>{name}</h5>
+            <h6>{username} </h6>
+            <h4>{phone} </h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Teacher;
