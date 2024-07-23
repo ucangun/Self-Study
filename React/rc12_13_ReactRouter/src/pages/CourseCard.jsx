4ff8364786f9ec6ca5056f012058b142d3a6ecde
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-23 09:25:22 +0200

    "Updated CourseCard component to use react-bootstrap components and render a list of courses with images, titles, and descriptions"

--- React/rc12_13_ReactRouter/src/pages/CourseCard.jsx
+++ React/rc12_13_ReactRouter/src/pages/CourseCard.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/esm/Col";
+import Button from "react-bootstrap/Button";
+// import {Button} from "react-bootstrap";
 
-const CourseCard = () => {
-  return <div>CourseCard</div>;
+//! react alanında döngü olarak sadece map desteklenir,condition lardan da sadece ternary desteklenir
+
+const CourseCard = ({ veri }) => {
+  console.log(veri);
+
+  //!alttaki ilk return react ın ekrana bastırılan kısmı
+  return (
+    <Container>
+      <Row className="g-3 text-center">
+        {veri.map(({ name, text, img, id }) => {
+          //!arrow (map) süslü kullandığında return ister.reactta süslü koymayabilirsiniz, o zaman returne de ihtiyaç olmaz
+
+          //?database den çekilen veriler ekrana bastırılırken, en dış div unique bir veri ister bunu da key={id} şeklinde yazarız. id olmak zorunda değil unique herhangi bir property olabilir, mesela img
+          return (
+            <Col
+              className="d-flex justify-content-center col-sm-12 col-md-6 col-lg-4"
+              key={id}
+            >
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                  <Card.Title>{name} </Card.Title>
+                  <Card.Text>{text}</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 };
 
 export default CourseCard;
