86c1eebb65ca789715027aec696074cf88089bd2
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-23 09:23:29 +0200

    "Added Button component and implemented ContactForm with form fields and submission functionality"

---
+++ React/rc12_13_ReactRouter/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button>LOGIN</button>
+      <button>OPEN</button>
+      <button>CLOSE</button>
+    </div>
+  );
+};
+
+export default Button;
--- React/rc12_13_ReactRouter/src/pages/ContactForm.jsx
+++ React/rc12_13_ReactRouter/src/pages/ContactForm.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const [info, setInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = info;
+
+  const handleInfo = (e) => {
+    console.log([e.target.id]);
+    setInfo({ ...info, [e.target.id]: e.target.value });
+  };
+
+  const sendToDatabase = (e) => {
+    e.preventDefault();
+    alert(`
+        name : ${name}
+        password : ${password}
+        email : ${email}
+    `);
+    setInfo({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="mt-4 p-3">
+      <div className="text-center">
+        <h1>*********************************</h1>
+        <h2>FORM EVENTS</h2>
+      </div>
+      <form onSubmit={sendToDatabase}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name: <span className="text-danger">{name} </span>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            required
+            onChange={handleInfo}
+            value={name}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email: <span className="text-danger">{email} </span>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            required
+            onChange={handleInfo}
+            value={email}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password: <span className="text-danger">{password} </span>
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            required
+            onChange={handleInfo}
+            value={password}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
