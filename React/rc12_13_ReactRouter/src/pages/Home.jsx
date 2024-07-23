ca6d46820ed7cfdb96f5a1cd56b8e86d02f5fb09
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-23 11:08:19 +0200

    "Added Slider and Button components to Home page"

--- React/rc12_13_ReactRouter/src/pages/Home.jsx
+++ React/rc12_13_ReactRouter/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Slider from "../components/Slider";
+import Button from "../components/Button";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Slider />
+      <Button />
+    </div>
+  );
 };
 
 export default Home;
