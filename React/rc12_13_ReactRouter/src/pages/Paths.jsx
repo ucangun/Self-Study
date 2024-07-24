597807d456d4aa82c15838af6c2f494db9dcdd48
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-24 11:19:13 +0200

    "Updated Paths component to include new UI elements and Outlet from react-router-dom"

--- React/rc12_13_ReactRouter/src/pages/Paths.jsx
+++ React/rc12_13_ReactRouter/src/pages/Paths.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Paths = () => {
-  return <div>Paths</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="container mt-4">
+      <h1>
+        Online IT Courses to Become a Qualified IT Professional with Clarusway
+      </h1>
+
+      <p className="fs-5">
+        Join outstanding companies with rewarding salaries. We offer the
+        highest-demand IT skills YOU need for success! CHOOSE THE BEST COURSE
+        FOR YOU Upgrade your career with the best online training led by top IT
+        experts!
+      </p>
+      <div>
+        <button className="btn btn-success w-25">FS</button>
+
+        <button className="btn btn-danger w-50">ana sayfa</button>
+        <button className="btn btn-warning w-25">Aws-Devops</button>
+      </div>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Paths;
