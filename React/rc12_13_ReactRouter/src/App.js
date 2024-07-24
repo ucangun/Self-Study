423cc5c1904018849045ebbda576e0d57842804c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-24 12:06:31 +0200

    "Refactored App.js to remove router configuration and import AppRouter instead"

--- React/rc12_13_ReactRouter/src/App.js
+++ React/rc12_13_ReactRouter/src/App.js
@@ -1,42 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MyNavbar from "./components/MyNavbar";
-import Teacher from "./pages/Teacher";
-import CourseCard from "./pages/CourseCard";
-import ContactForm from "./pages/ContactForm";
-import Paths from "./pages/Paths";
-import TeacherDetails from "./pages/TeacherDetails";
-import NotFound from "./pages/NotFound";
-import CardDetails from "./pages/CardDetails";
-import FS from "./pages/FS";
-import AWS from "./pages/AWS";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div>
-      <Router>
-        <MyNavbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          {/* "/" (ana yol) tüm yollara dahil edilmiştir, bu nedenle onu "/" ile başlayan diğer yollardan ayırt etmek için exact anahtar kelimesine sahip olması gerekir . */}
-          <Route path="/teacher" element={<Teacher />} />
-          <Route path="/teacher/:id" element={<TeacherDetails />} />
-
-          <Route path="/courses" element={<CourseCard />} />
-          <Route path="/courses/:name" element={<CardDetails />} />
-
-          <Route path="/contact" element={<ContactForm />} />
-
-          <Route path="/paths" element={<Paths />}>
-            <Route path="/paths/fs" element={<FS />} />
-            <Route path="aws" element={<AWS />} />
-          </Route>
-
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <AppRouter />
     </div>
   );
 }
