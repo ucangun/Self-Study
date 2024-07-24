7d01d047207ff00395d6dabff489d0cd4681374b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-24 11:11:51 +0200

    "Updated NotFound component to use react-router-dom's useNavigate hook for navigation"

--- React/rc12_13_ReactRouter/src/pages/NotFound.jsx
+++ React/rc12_13_ReactRouter/src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import notFound from "../img/notFound.jpeg";
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <Container className="text-center">
       <img src={notFound} alt="" width="500px" />
       <div>
-        <button className="btn btn-info">GO HOME</button>
+        <button onClick={() => navigate("/")} className="btn btn-info">
+          GO HOME
+        </button>
       </div>
     </Container>
   );
