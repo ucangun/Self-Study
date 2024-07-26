c03f6bd6d37e0ed16fe43fd8a40548c821da8121
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-26 11:01:02 +0200

    "Updated CardTotal and ProductCard components to receive props, added error handling and loading state to ProductList page, and refactored JSX elements"

--- React/rc14_Shopping-Card/src/pages/ProductList.jsx
+++ React/rc14_Shopping-Card/src/pages/ProductList.jsx
@@ -1,31 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
 
 const ProductList = () => {
   const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
 
   const [urunler, setUrunler] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getData = async () => {
-    const { data } = await axios.get(BASE_URL);
-    setUrunler(data);
+    try {
+      const { data } = await axios.get(BASE_URL);
+      setUrunler(data);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
+  if (error) {
+    return <p>Something went wrong...</p>;
+  }
+
   return (
     <div className="container mt-3">
       <div className="d-sm-block d-md-flex">
-        {"" ? (
+        {loading ? (
           <p>Loading.....</p>
         ) : (
           <>
             <article id="product-panel" className="col-md-6">
-              {/* diziyi bastır */}
+              {urunler.map((urun) => (
+                <ProductCard key={urun.id} urun={urun} />
+              ))}
             </article>
-            <article></article>
+            <article>
+              <CardTotal urunler={urunler} />
+            </article>
           </>
         )}
       </div>
