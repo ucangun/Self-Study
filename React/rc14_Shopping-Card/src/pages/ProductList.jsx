5c82dd5dcb7a5098ece54d77c6f376850a976144
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-26 10:02:24 +0200

    "Added axios import and implemented data fetching from API in ProductList component"

--- React/rc14_Shopping-Card/src/pages/ProductList.jsx
+++ React/rc14_Shopping-Card/src/pages/ProductList.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductList = () => {
   const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
 
+  const [urunler, setUrunler] = useState([]);
 
-  
+  const getData = async () => {
+    const { data } = await axios.get(BASE_URL);
+    setUrunler(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="container mt-3">
@@ -14,11 +23,9 @@ const ProductList = () => {
         ) : (
           <>
             <article id="product-panel" className="col-md-6">
-            {/* diziyi bastır */}
+              {/* diziyi bastır */}
             </article>
-            <article>
-             
-            </article>
+            <article></article>
           </>
         )}
       </div>
