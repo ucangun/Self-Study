c7d5351ba624a73667c1ce892aec60c94fc51364
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-13 09:28:52 +0200

    add project files

---
+++ React/rc16_Styled_Components/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Styled Components</h1>
+      <a href="#">LINK</a>
+      <button>TIKLA 1</button>
+      <button>TIKLA 2</button>
+      <button>TIKLA 3</button>
+      <button>TIKLA 4</button>
+      <button>TIKLA 5</button>
+    </div>
+  );
+};
+
+export default Home;
