bc5a08eb1ef68edec15866db82d729169907cb4d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-15 11:17:38 +0200

    "Added styled components for Header and updated RecipeProvider with state and API call for recipe data"

--- React/rc17-18-RecipeAppProject/src/context/RecipeProvider.jsx
+++ React/rc17-18-RecipeAppProject/src/context/RecipeProvider.jsx
@@ -1,6 +1,7 @@
 //! Context Alani create edelim
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
 
 export const RecipeContext = createContext();
 
@@ -8,10 +9,31 @@ const RecipeProvider = ({ children }) => {
   //! login ve privaterouter sayfalarinda gerekli usestateleri actik
 
   const [name, setName] = useState(localStorage.getItem("username") || "");
-
   const [password, setPassword] = useState(
     localStorage.getItem("password") || ""
   );
+
+  //! Home , Header , Recipecard da gerekli olan veriler
+
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState("");
+  const [mealType, setMealType] = useState("Breakfast");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const APP_ID = "633135cc";
+  const APP_KEY = "4e352366e820300289821caacf64d308";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${mealType}`;
+
+  const getData = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+    setRecipes(data.hits);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <RecipeContext.Provider value={{ name, setName, password, setPassword }}>
