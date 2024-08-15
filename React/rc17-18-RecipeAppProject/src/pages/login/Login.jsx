37ed18e33ed881f821311eb5b5cdc07fbc848096
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-15 10:41:06 +0200

    "Updated Login component to handle form submission and navigation, added PrivateRouter component to restrict access to Home page, and modified AppRouter to use PrivateRouter"

--- React/rc17-18-RecipeAppProject/src/pages/login/Login.jsx
+++ React/rc17-18-RecipeAppProject/src/pages/login/Login.jsx
@@ -8,35 +8,45 @@ import {
   StyledImg,
   StyledInput,
 } from "./LoginStyles";
-
 import mealFoto from "../../assets/meal.svg";
 import { useContext } from "react";
 import { RecipeContext } from "../../context/RecipeProvider";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { name, password, setName, setPassword } = useContext(RecipeContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("username", name);
+    localStorage.setItem("password", password);
+    navigate("/home");
+  };
 
   return (
     <LoginContainer>
       <FormContainer>
         <StyledImg src={mealFoto} />
         <Header>{"<Clarusway>"} Recipe</Header>
-        <StyledForm></StyledForm>
-        <StyledInput
-          type="text"
-          placeholder="username"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <StyledInput
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <StyledButton>Login</StyledButton>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput
+            type="text"
+            placeholder="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <StyledInput
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
       </FormContainer>
     </LoginContainer>
   );
--- React/rc17-18-RecipeAppProject/src/router/AppRouter.jsx
+++ React/rc17-18-RecipeAppProject/src/router/AppRouter.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
