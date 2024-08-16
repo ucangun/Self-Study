1d18dbc319d6fd80b673d485826449b2179ad4ec
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-16 12:19:53 +0200

    "Updated NotFound component to include countdown timer and redirect to home page, added NotFoundStyle component for styling"

--- React/rc17-18-RecipeAppProject/src/pages/errorPage/NotFound.jsx
+++ React/rc17-18-RecipeAppProject/src/pages/errorPage/NotFound.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Main from "./NotFoundStyle";
+import { Navigate } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const [count, setCount] = useState(5);
 
-export default NotFound
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => (count === 0 ? count : count - 1));
+    }, 1000);
+    if (count === 0) clearInterval(timer);
+  }, []);
+
+  if (count === 0) {
+    return <Navigate to="/home" />;
+  }
+
+  return (
+    <Main>
+      <h3>Something went wrong...</h3>
+      <p>
+        Within <span>{count}</span> seconds. you will be redirected to the home
+        page!
+      </p>
+    </Main>
+  );
+};
+
+export default NotFound;
