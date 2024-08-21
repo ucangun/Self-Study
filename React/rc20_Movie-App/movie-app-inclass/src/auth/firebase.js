a93941dfca6da6430ff7fe1f378788d436602c3b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-21 09:54:22 +0200

    "Removed empty lines and added environment variables to firebaseConfig"

--- React/rc20_Movie-App/movie-app-inclass/src/auth/firebase.js
+++ React/rc20_Movie-App/movie-app-inclass/src/auth/firebase.js
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
@@ -13,10 +11,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth= getAuth(app)
+export const auth = getAuth(app);
