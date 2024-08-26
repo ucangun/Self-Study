099f57ef3663466aa660c137bb0072302468280e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-26 10:43:03 +0200

    "Updated TypoButtons component with Material UI elements and styling, and imported it into Home page."

--- React/rc21_Material_UI/src/components/TypoButtons.jsx
+++ React/rc21_Material_UI/src/components/TypoButtons.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Box, Container, Button, Typography } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const TypoButtons = () => {
   return (
-    <div>TypoButtons</div>
-  )
-}
+    <div>
+      {/* maxWidth="md" width en fazla md büyüklükte olsun.  gutterBottom={true}
+      alttan margin. component attribute ü  sadece console da etkilidir , görüntü yine variant neyse odur*/}
+      <Container maxWidth="md">
+        <Typography
+          variant="h2"
+          component="h3"
+          gutterBottom
+          align="center"
+          color="error"
+          sx={{
+            background: "#eee",
+            mt: 4,
+            border: "1px solid red",
+          }}
+        >
+          Typography Container Box Button
+        </Typography>
+        {/* body1: p etiketidir,body1=16px daha büyük fs ,body2=14px daha küçük font lu.  button=span etiketidir+büyük harf */}
+        <Typography variant="button" gutterBottom>
+          SPAN Lorem, ipsum.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          P TAG BODY 1 Lorem, ipsum dolor.
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          P TAG BODY 2 Lorem, ipsum dolor.
+        </Typography>
+      </Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 4,
+          gap: 2,
+        }}
+      >
+        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button variant="contained" color="success" endIcon={<SendIcon />}>
+          Send
+        </Button>
+      </Box>
+    </div>
+  );
+};
 
-export default TypoButtons
+export default TypoButtons;
--- React/rc21_Material_UI/src/pages/Home.jsx
+++ React/rc21_Material_UI/src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import TypoButtons from '../components/TypoButtons'
-import CardGrid from '../components/CardGrid'
-import Navbar from '../components/Navbar'
-import TextFieldComp from '../components/TextFieldComp'
+import React from "react";
+import TypoButtons from "../components/TypoButtons";
+import CardGrid from "../components/CardGrid";
+import Navbar from "../components/Navbar";
+import TextFieldComp from "../components/TextFieldComp";
 
 const Home = () => {
   return (
     <>
-    home
+      <TypoButtons />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
