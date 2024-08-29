17ae7f880097c8e8f4348c281ccb4aa62dbfb58c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-29 10:00:47 +0200

    "Added React Router DOM and implemented basic routing with a login route"

--- React/rc23_redux-toolkit-example/src/router/AppRouter.jsx
+++ React/rc23_redux-toolkit-example/src/router/AppRouter.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
