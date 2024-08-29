ba1a3e5ecf9f658c11f08826af757e7b07d4f449
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-29 09:35:55 +0200

    "Added imports and configured store with reducers in store.jsx"

--- React/rc23_redux-toolkit-example/src/app/store.jsx
+++ React/rc23_redux-toolkit-example/src/app/store.jsx
@@ -1,2 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import yetkiSLice from ".,/features/yetkiSlice";
+import haberSlice from ".,/features/haberSlice";
 
-
+export const store = configureStore({
+  reducer: {
+    yetkiSlice: yetkiSLice,
+    haberSlice: haberSlice,
+  },
+});
