5ea55bcec63bb903be44b3a559a448c1617ce53c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-29 11:18:30 +0200

    "Refactor haberSlice to use Redux Toolkit and update News component to use useSelector and useDispatch"

--- React/rc23_redux-toolkit-example/src/features/haberSlice.jsx
+++ React/rc23_redux-toolkit-example/src/features/haberSlice.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const haberSlice = () => {
-  return (
-    <div>haberSlice</div>
-  )
-}
+export const getData = createAsyncThunk("haberSlice/getData", async () => {
+  const { data } = await axios(
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb"
+  );
 
-export default haberSlice
+  return data.articles;
+});
+
+export const haberSlice = createSlice({
+  name: "haberSlice",
+  initialState: {
+    haberler: [],
+    loading: true,
+  },
+
+  reducers: {
+    clear: (state) => {
+      state.haberler = [];
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.haberler = action.payload;
+      })
+      .addCase(getData.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export const { clear } = haberSlice.actions;
+
+export default haberSlice.reducer;
--- React/rc23_redux-toolkit-example/src/pages/News.jsx
+++ React/rc23_redux-toolkit-example/src/pages/News.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Box,
   Button,
@@ -8,42 +8,42 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+
 const News = () => {
+  const { haberler, loading } = useSelector((state) => state.haberSlice);
+
   return (
     <>
-    
-        <Box
-          xs={{ d: "flex" }}
-          display="flex"
-          alignItems="center"
-          justifyContent="space-evenly"
-          flexWrap="wrap"
-        >
-          {[].map((a, index) => (
-            <Card sx={{ maxWidth: 345, maxHeight: 600, m: 5 }} key={index}>
-              <CardMedia component="img" image={""} height="250" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" >
-                  CLEAR
-                </Button>
-                <Button size="small" href={""} target="_blank">
-                  DETAIL
-                </Button>
-              </CardActions>
-            </Card>
-          ))}
-        </Box>
-      
+      <Box
+        xs={{ d: "flex" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-evenly"
+        flexWrap="wrap"
+      >
+        {haberler.map((a, index) => (
+          <Card sx={{ maxWidth: 345, maxHeight: 600, m: 5 }} key={index}>
+            <CardMedia component="img" image={""} height="250" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">CLEAR</Button>
+              <Button size="small" href={""} target="_blank">
+                DETAIL
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Box>
     </>
   );
-}
+};
 
-export default News
+export default News;
