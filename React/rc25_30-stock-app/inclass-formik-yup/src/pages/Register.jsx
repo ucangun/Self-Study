d14f599223eacf60cc873b5a430a38592ecf6760
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-02 12:58:21 +0200

    "Added password validation rules and wrapped form fields with Box component, also added a submit button"

--- React/rc25_30-stock-app/inclass-formik-yup/src/pages/Register.jsx
+++ React/rc25_30-stock-app/inclass-formik-yup/src/pages/Register.jsx
@@ -6,7 +6,7 @@ import image from "../assets/regi.avif";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
 import { Formik, Form } from "formik";
@@ -25,6 +25,13 @@ const SignupSchema = Yup.object().shape({
     .max(50, "Too Long!")
     .required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .required()
+    .min(8)
+    .matches(/\d+/, "Digit karakter icermelidir!")
+    .matches(/[a-z]/, "Kücük harf icermelidir!")
+    .matches(/[A-Z]/, "Büyük harf icermelidir!")
+    .matches(/[@$?!%&*]+/, "Özel karakter icermelidir(@$?!%&*)"),
 });
 
 const Register = () => {
@@ -87,62 +94,67 @@ const Register = () => {
               /* and other goodies */
             }) => (
               <Form>
-                <TextField
-                  name="username" //formik name attributedından eşleştirme yapıyor.
-                  label="Username"
-                  value={values.username}
-                  onChange={handleChange}
-                  onBlur={handleBlur} // kullanıcının input alanından ayrıldığını yaklayan event
-                  error={touched.username && Boolean(errors.username)} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için boolean deger döndürüyoruz.
-                  // touched da kullanıcının inputa tıklayıp tıklamadığını yakalıyor
-                  helperText={touched.username && errors.username} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
-                />
-                {/* error ve helperText propertyleri Textfield componentine ait propertyler. */}
-                {/* mui textfield kullanmadığımzda <span>{touched.username && errors.username}</span> */}
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    name="username" //formik name attributedından eşleştirme yapıyor.
+                    label="Username"
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur} // kullanıcının input alanından ayrıldığını yaklayan event
+                    error={touched.username && Boolean(errors.username)} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için boolean deger döndürüyoruz.
+                    // touched da kullanıcının inputa tıklayıp tıklamadığını yakalıyor
+                    helperText={touched.username && errors.username} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
+                  />
+                  {/* error ve helperText propertyleri Textfield componentine ait propertyler. */}
+                  {/* mui textfield kullanmadığımzda <span>{touched.username && errors.username}</span> */}
 
-                <TextField
-                  id="firstName"
-                  name="firstName"
-                  label="FirstName"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.firstName && Boolean(errors.firstName)}
-                  helperText={touched.firstName && errors.firstName}
-                />
-                <TextField
-                  id="lastName"
-                  name="lastName"
-                  label="LastName"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.lastName && Boolean(errors.lastName)}
-                  helperText={touched.lastName && errors.lastName}
-                />
+                  <TextField
+                    id="firstName"
+                    name="firstName"
+                    label="FirstName"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.firstName && Boolean(errors.firstName)}
+                    helperText={touched.firstName && errors.firstName}
+                  />
+                  <TextField
+                    id="lastName"
+                    name="lastName"
+                    label="LastName"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.lastName && Boolean(errors.lastName)}
+                    helperText={touched.lastName && errors.lastName}
+                  />
 
-                <TextField
-                  id="email"
-                  name="email"
-                  label="Email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.email && Boolean(errors.email)}
-                  helperText={touched.email && errors.email}
-                />
-                <TextField
-                  id="password"
-                  name="password"
-                  type="password"
-                  label="Password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.password && Boolean(errors.password)}
-                  helperText={touched.password && errors.password}
-                />
+                  <TextField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    id="password"
+                    name="password"
+                    type="password"
+                    label="Password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                  <Button type="submit" variant="contained">
+                    Sign Up
+                  </Button>
+                </Box>
               </Form>
             )}
           </Formik>
