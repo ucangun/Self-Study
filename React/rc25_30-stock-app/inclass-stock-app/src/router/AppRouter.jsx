c89126d6975e98034097630415623ea62869eab8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-04 11:18:16 +0200

    "Added new routes for Home, Brands, Firms, Products, Purchases, and Sales pages, and updated AppRouter component"

--- React/rc25_30-stock-app/inclass-stock-app/src/router/AppRouter.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/router/AppRouter.jsx
@@ -3,15 +3,33 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Brands from "../pages/Brands";
+import Firms from "../pages/Firms";
+import Products from "../pages/Products";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="register" element={<Register />} />        
+        <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            {/* <Route path="" element={<Home />} /> */}
+            {/* nested route yapılarında index dediğimzde boş path vermemize gerek kalmaz. kapsayıcısının pathi geldğinde çalışır */}
+            <Route index element={<Home />} />
+            {/* Absolute Path Kullanımı */}
+            {/* <Route path="/brands" element={<Brands />} /> // bu şekilde bırakırsam bana şu hatayı verecektir=> Absolute route path "/products" nested under path "/stock/" is not valid. An absolute child route path must start with the combined path of all its parent routes. */}
+            <Route path="/stock/brands" element={<Brands />} />
+            {/* Relative Path Kullanımı */}
+            <Route path="firms" element={<Firms />} />
+            <Route path="products" element={<Products />} />
+            <Route path="purchases" element={<Purchases />} />
+            <Route path="sales" element={<Sales />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
