b3d607b6a49fafdbef5f3d1fdce76140a3e12924
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-04 12:37:53 +0200

    "Updated MenuListItems component to use an array of links with titles and URLs, and added navigation using react-router-dom"

--- React/rc25_30-stock-app/inclass-stock-app/src/components/MenuListItems.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/components/MenuListItems.jsx
@@ -11,25 +11,52 @@ import ListItemText from "@mui/material/ListItemText";
 import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
+import { useNavigate } from "react-router-dom";
+
+const links = [
+  {
+    title: "Dashboard",
+    url: "/stock",
+  },
+  {
+    title: "Purchases",
+    url: "purchases",
+  },
+  {
+    title: "Sales",
+    url: "sales",
+  },
+  {
+    title: "Firms",
+    url: "firms",
+  },
+  {
+    title: "Brands",
+    url: "brands",
+  },
+  {
+    title: "Products",
+    url: "products",
+  },
+];
 
 const MenuListItems = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {["Dashboard", "Purchases", "Sales", "Brands", "Products"].map(
-          (text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          )
-        )}
+        {links.map((item, index) => (
+          <ListItem key={item.title} disablePadding>
+            <ListItemButton onClick={() => navigate(item.url)}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
       </List>
     </div>
   );
