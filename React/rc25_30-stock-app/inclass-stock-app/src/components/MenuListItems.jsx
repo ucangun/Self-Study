ee156c375f278108b5c34556a3ecf8e057837a84
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-05 09:27:50 +0200

    "Updated MenuListItems component: changed links titles and URLs, added icon property to links, and modified ListItemButton to display icons from /assets/navbar directory."

--- React/rc25_30-stock-app/inclass-stock-app/src/components/MenuListItems.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/components/MenuListItems.jsx
@@ -12,31 +12,41 @@ import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/material";
+
+const icon = (name) => `/assets/navbar/${name}.svg`;
 
 const links = [
   {
     title: "Dashboard",
     url: "/stock",
-  },
-  {
-    title: "Purchases",
-    url: "purchases",
-  },
-  {
-    title: "Sales",
-    url: "sales",
+    // icon:"/assets/navbar/ic_analytics.svg",
+    icon: icon("ic_analytics"),
   },
   {
     title: "Firms",
-    url: "firms",
+    url: "/stock/firms",
+    icon: icon("firms"),
   },
   {
     title: "Brands",
-    url: "brands",
+    url: "/stock/brands",
+    icon: icon("brand"),
+  },
+  {
+    title: "Purchases",
+    url: "/stock/purchases",
+    icon: icon("purchase"),
+  },
+  {
+    title: "Sales",
+    url: "/stock/sales",
+    icon: icon("sales"),
   },
   {
     title: "Products",
-    url: "products",
+    url: "/stock/products",
+    icon: icon("ic_cart"),
   },
 ];
 
@@ -50,9 +60,31 @@ const MenuListItems = () => {
         {links.map((item, index) => (
           <ListItem key={item.title} disablePadding>
             <ListItemButton onClick={() => navigate(item.url)}>
-              <ListItemIcon>
+              {/* <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
+              </ListItemIcon> */}
+              {/* <Box 
+                sx={{
+                  backgroundImage: `url(${item.icon})`,
+                  backgroundPosition:"center",
+                  backgroundSize:"cover",
+                  backgroundRepeat:"no-repeat",
+                  width:"24px",
+                  height:"24px",
+                  backgroundColor:"red",
+                  mr:2
+                }}
+             /> */}
+
+              <Box
+                sx={{
+                  width: "24px",
+                  height: "24px",
+                  mr: 2,
+                  mask: `url(${item.icon}) no-repeat center / contain `,
+                  bgcolor: "red",
+                }}
+              />
               <ListItemText primary={item.title} />
             </ListItemButton>
           </ListItem>
