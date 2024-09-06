1655028024125d900744f57c0949882b7cc2bee4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-06 10:00:44 +0200

    add StockSlice

---
+++ React/rc25_30-stock-app/inclass-stock-app/src/features/stockSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stockSlice = createSlice({
+  name: "stock",
+
+  initialState: {
+    loading: false,
+    error: false,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { fetchStart, fetchFail } = stockSlice.actions;
+export default stockSlice.reducer;
