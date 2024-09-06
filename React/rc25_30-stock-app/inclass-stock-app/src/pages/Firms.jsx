37ca58a6869b4cf3df8cc1a594c2e5dabf0c2dac
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-06 12:39:37 +0200

    "Updated Firms.jsx to import additional components, use react-redux's useSelector, and render a list of FirmCards with a button and typography"

--- React/rc25_30-stock-app/inclass-stock-app/src/pages/Firms.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/pages/Firms.jsx
@@ -1,14 +1,38 @@
 import React, { useEffect } from "react";
 import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import Container from "@mui/material/Container";
+import { Button, Grid, Typography } from "@mui/material";
+import FirmCard from "../components/Cards/FirmCard";
 
 const Firms = () => {
   const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
     getStockData("firms");
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <Container>
+      <Typography
+        align="center"
+        color="secondary.second"
+        variant="h4"
+        component="h1"
+      >
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+      <Grid container spacing={2} mt={2}>
+        {firms.map((firm) => (
+          <Grid item xs={12} md={6} lg={4} xl={3} key={firm._id}>
+            <FirmCard />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Firms;
