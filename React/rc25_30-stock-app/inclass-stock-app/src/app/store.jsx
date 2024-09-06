35159b68f001ed0414b03f649d5948b7ca84b8dd
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-06 09:16:51 +0200

    "Added redux-persist to store, updated store configuration and exported persistor"

--- React/rc25_30-stock-app/inclass-stock-app/src/app/store.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/app/store.jsx
@@ -1,12 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-// import authMidReducer from "../features/authSliceMiddleware"; //!middleware ile olan kullanım
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    // auth:authMidReducer,
+    // auth: authReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export let persistor = persistStore(store);
+
 export default store;
