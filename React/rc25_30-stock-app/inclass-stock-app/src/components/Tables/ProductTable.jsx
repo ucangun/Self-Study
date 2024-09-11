45b33099ba12d39a7016edc230ffe64a5de0d915
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-11 12:09:59 +0200

    "Updated ProductTable component to use Redux state for data, changed column fields and added value getters, and removed hardcoded rows data"

--- React/rc25_30-stock-app/inclass-stock-app/src/components/Tables/ProductTable.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/components/Tables/ProductTable.jsx
@@ -1,55 +1,72 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: "_id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
+    field: "categoryId",
+    headerName: "Category",
     width: 150,
-    editable: true,
+    // editable: true,
+    valueGetter: (value) => value?.name ?? "-NoCategory",
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: "brandId",
+    headerName: "Brand",
     width: 150,
-    editable: true,
+    // editable: true,
+    valueGetter: (value) => value?.name || "-NoBrand",
   },
   {
-    field: "age",
-    headerName: "Age",
+    field: "name",
+    headerName: "Product Name",
+    // type: "number",
+    width: 120,
+    // editable: true,
+  },
+  {
+    field: "quantity",
+    headerName: "Stock",
     type: "number",
     width: 110,
-    editable: true,
+    // editable: true,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
+    field: "actions",
+    headerName: "Action",
+    // description: "This column has a value getter and is not sortable.",
     sortable: false,
-    width: 160,
+    width: 110,
     valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
+
+function getRowId(row) {
+  console.log(row);
+  return row._id;
+}
 
 export default function ProductTable() {
+  const { products } = useSelector((state) => state.stock);
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        getRowId={getRowId}
+        rows={products}
         columns={columns}
         initialState={{
           pagination: {
