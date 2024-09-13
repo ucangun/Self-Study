eae3504d2c879c6b323d99e4859b239a99fef4b6
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-13 09:00:21 +0200

    "Deleted PurchaseModal.jsx and PurchaseTable.jsx files, and modified useStockCall.jsx to remove unused functions and add new ones."

--- React/rc25_30-stock-app/inclass-stock-app/src/hooks/useStockCall.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/hooks/useStockCall.jsx
@@ -1,9 +1,14 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchFail,
+  // brandSuccess,
   fetchStart,
-  getStockSuccess,
   getProCatBrandSuccess,
+  getProPurcFirBrandsSuccess,
+  getProSalBrandsSuccess,
+  // firmSuccess,
+  getStockSuccess,
 } from "../features/stockSlice";
 import axios from "axios";
 import useAxios from "./useAxios";
@@ -13,39 +18,68 @@ const useStockCall = () => {
   const token = useSelector((state) => state.auth.token);
   const axiosWithToken = useAxios();
 
+  //   const getFirms = async () => {
+  //     dispatch(fetchStart());
+  //     try {
+  //       const { data } = await axios(`${import.meta.env.VITE_BASE_URL}firms`, {
+  //         headers: {
+  //           Authorization: `Token ${token}`,
+  //         },
+  //       });
+  //       console.log(data);
+  //       dispatch(firmSuccess(data));
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   const getBrands = async () => {
+  //     dispatch(fetchStart());
+  //     try {
+  //       const { data } = await axios(`${import.meta.env.VITE_BASE_URL}brands`, {
+  //         headers: {
+  //           Authorization: `Token ${token}`,
+  //         },
+  //       });
+  //       console.log(data);
+  //       dispatch(brandSuccess(data));
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //* DRY
+  //! yukarıdaki gibi her seferinde yazmak yerine daha modüler bir yapı kurarak tek bir fonksiyonla bir den fazla get işlemini gerçekleştirebiliyoruz
   const getStockData = async (endpoint) => {
     dispatch(fetchStart());
     try {
-      /*
-      const { data } = await axios(
-        `${import.meta.env.VITE_BASE_URL}${endpoint}`,
-        {
-          headers: {
-            Authorization: `Token ${token}`,
-          },
-        }
-      );
-      */
+      // const { data } = await axios(
+      //   `${import.meta.env.VITE_BASE_URL}${endpoint}`,
+      //   {
+      //     headers: {
+      //       Authorization: `Token ${token}`,
+      //     },
+      //   }
+      // );
       const { data } = await axiosWithToken.get(endpoint);
       console.log(data);
-      dispatch(getStockSuccess({ stock: data.data, endpoint }));
+      dispatch(getStockSuccess({ stock: data.data, endpoint })); //* action creatorlar her zaman tek bir parametre kabul ederler
     } catch (error) {
       console.log(error);
-      dispatch(fetchFail());
     }
   };
-
+  //! istek atarken ortak olan base_url  ve token gibi değerleri her seferinde yazmak yerine axios instance kullanarak bunları orada tanımlıyoruz. Ve sonrasında sadece istek atmak istediğimiz end pointi yazmamız yeterli oluyor.
+  //? https://axios-http.com/docs/instance
   const deleteStockData = async (endpoint, id) => {
     dispatch(fetchStart());
     try {
-      /*
-      await axios.delete(`${import.meta.env.VITE_BASE_URL}${endpoint}/${id}`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
-      */
+      // await axios.delete(`${import.meta.env.VITE_BASE_URL}${endpoint}/${id}`,
+      //   {
+      //     headers: {
+      //       Authorization: `Token ${token}`,
+      //     },
+      //   })
       await axiosWithToken.delete(`${endpoint}/${id}`);
+      // getStockData(endpoint)
     } catch (error) {
       console.log(error);
       dispatch(fetchFail());
@@ -81,14 +115,19 @@ const useStockCall = () => {
     }
   };
 
+  //!Promise.all()
+  //* eş zamanlı istek atma. aynı anda istek atılıyor aynı anda responselar gelmeye başlıyor. Zaman noktasında da avantajlı. En uzun hangi istek sürdüyse veriler ondan sonra valid olur. Birbirine bağımlı isteklerde en büyük avantajı hata durumu. İsteklerden biri bile hatalı olursa hepsi iptal olur.
   const getProCatBrand = async () => {
     dispatch(fetchStart());
     try {
+      // const [a,b] = [1,2] // array descturing
       const [products, categories, brands] = await Promise.all([
         axiosWithToken("products"),
         axiosWithToken("categories"),
         axiosWithToken("brands"),
       ]);
+      console.log("products", products);
+
       dispatch(
         getProCatBrandSuccess([
           products?.data?.data,
@@ -96,19 +135,61 @@ const useStockCall = () => {
           brands?.data?.data,
         ])
       );
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+  const getProSalBrands = async () => {
+    dispatch(fetchStart());
+    try {
+      // const { data } = await axiosWithToken.get(`stock/${url}/`);
+      const [products, brands, sales] = await Promise.all([
+        axiosWithToken.get(`products/`),
+        axiosWithToken.get(`brands/`),
+        axiosWithToken.get(`sales/`),
+      ]);
 
-      console.log(products);
+      dispatch(
+        getProSalBrandsSuccess([products?.data, brands?.data, sales?.data])
+      );
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+  const getProPurcFirBrands = async () => {
+    dispatch(fetchStart());
+    try {
+      // const { data } = await axiosWithToken.get(`stock/${url}/`);
+      const [products, purchases, firms, brands] = await Promise.all([
+        axiosWithToken.get(`products/`),
+        axiosWithToken.get(`purchases/`),
+        axiosWithToken.get(`firms/`),
+        axiosWithToken.get(`brands/`),
+      ]);
+
+      dispatch(
+        getProPurcFirBrandsSuccess([
+          products?.data,
+          purchases?.data,
+          firms?.data,
+          brands?.data,
+        ])
+      );
     } catch (error) {
       dispatch(fetchFail());
     }
   };
 
   return {
+    // getFirms,
+    // getBrands,
     getStockData,
     deleteStockData,
     postStockData,
     putStockData,
     getProCatBrand,
+    getProSalBrands,
+    getProPurcFirBrands,
   };
 };
 
