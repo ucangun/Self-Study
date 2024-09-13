2fb27538e4fa547624d41778cd6952c8476ee229
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-13 08:54:33 +0200

    add sales page

--- React/rc25_30-stock-app/inclass-stock-app/src/pages/Sales.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/pages/Sales.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import useStockCall from "../hooks/useStockCall";
+import { Button, Container, Typography } from "@mui/material";
+
+import SaleModal from "../components/Modals/SaleModal";
+import SaleTable from "../components/Tables/SaleTable";
 
 const Sales = () => {
-  return <div>Sales</div>;
+  const { getProSalBrands } = useStockCall();
+
+  const [open, setOpen] = useState(false);
+
+  const [initialState, setInitialState] = useState({
+    brandId: "",
+    productId: "",
+    quantity: "",
+    price: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      brandId: "",
+      productId: "",
+      quantity: "",
+      price: "",
+    });
+  };
+
+  useEffect(() => {
+    getProSalBrands();
+  }, []); // eslint-disable-line
+
+  return (
+    <Container maxWidth={"xl"}>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Sales
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Sale
+      </Button>
+
+      {open && (
+        <SaleModal
+          open={open}
+          handleClose={handleClose}
+          initialState={initialState}
+        />
+      )}
+      <SaleTable handleOpen={handleOpen} setInitialState={setInitialState} />
+    </Container>
+  );
 };
 
 export default Sales;
