4405754c00d6c8fbe8d768d853be0c99eab36811
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-13 08:52:34 +0200

    "Refactor Purchases component to include state, effects, and modals"

--- React/rc25_30-stock-app/inclass-stock-app/src/pages/Purchases.jsx
+++ React/rc25_30-stock-app/inclass-stock-app/src/pages/Purchases.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import useStockCall from "../hooks/useStockCall";
+import PurchaseModal from "../components/Modals/PurchaseModal";
+import PurchaseTable from "../components/Tables/PurchaseTable";
+import { Button, Container, Typography } from "@mui/material";
 
 const Purchases = () => {
-  return <div>Purchases</div>;
+  const { getProPurcFirBrands } = useStockCall();
+
+  const [open, setOpen] = useState(false);
+
+  const [initialState, setInitialState] = useState({
+    brandId: "",
+    firmId: "",
+    productId: "",
+    quantity: "",
+    price: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      brandId: "",
+      firmId: "",
+      productId: "",
+      quantity: "",
+      price: "",
+    });
+  };
+
+  useEffect(() => {
+    getProPurcFirBrands();
+  }, []); // eslint-disable-line
+
+  return (
+    <Container maxWidth={"xl"}>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Purchases
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Purchase
+      </Button>
+
+      {open && (
+        <PurchaseModal
+          open={open}
+          handleClose={handleClose}
+          initialState={initialState}
+        />
+      )}
+
+      <PurchaseTable
+        setInitialState={setInitialState}
+        handleOpen={handleOpen}
+      />
+    </Container>
+  );
 };
 
 export default Purchases;
