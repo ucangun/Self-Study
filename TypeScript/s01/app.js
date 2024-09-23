5c5f5a89fd80912bca7c969b39d172a57c3157af
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-23 11:03:42 +0200

    tsc app.ts

--- TypeScript/s01/app.js
+++ TypeScript/s01/app.js
@@ -1,1 +1,11 @@
 console.log("Umut Can");
+var b = 5;
+var c = 2;
+// b = "5";
+var e = "Hello";
+var f = "World!";
+var g;
+var h;
+var x; //
+x = "Hello";
+y = "World";
--- TypeScript/s01/app.ts
+++ TypeScript/s01/app.ts
@@ -1,1 +1,17 @@
-console.log("Umut Can");
+console.log("Cohort De09");
+
+let b = 5;
+let c = 2;
+
+// b = "5" //Type 'string' is not assignable to type 'number'.
+
+let e: string = "Hello";
+let f = "World!";
+
+let g: boolean;
+let h; // any atar otomatik olarak
+
+let x, y, z: string; // tek satırda birden fazla değişkene type belirtemiyoruz, bastakileri any olarak kabul ediyor.
+x = "Merhaba";
+y = 5;
+z = "Hey";
