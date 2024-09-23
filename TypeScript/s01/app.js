a5fa507ea67dccd61689f4174cc14ad337b37fc8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-23 11:16:33 +0200

    "Updated app.js and app.ts files with new variable declarations, type annotations, and array operations."

--- TypeScript/s01/app.js
+++ TypeScript/s01/app.js
@@ -1,11 +1,39 @@
-console.log("Umut Can");
+console.log("Cohort De09");
 var b = 5;
 var c = 2;
-// b = "5";
-var e = "Hello";
-var f = "World!";
-var g;
-var h;
-var x; //
-x = "Hello";
-y = "World";
+// b = "5" //Type 'string' is not assignable to type 'number'.
+/*
+let e : string = 'Hello'
+let f = "World!"
+
+let g : boolean;
+let h; // any atar otomatik olarak
+
+let x,y,z: string; // tek satırda birden fazla değişkene type belirtemiyoruz, baştakileri any olarak kabul ediyor sonrakine asıl tip ataması yapıyor.
+x="Merhaba"
+y=5
+z = "Oldu"
+z = 5 // error
+*/
+//* Arrays
+/*
+let nums: number[] = [1, 2, "Hi"];
+
+nums.push("Merhaba");
+nums.push(5);
+nums.push(false);
+
+console.log(nums);
+
+nums.forEach((item) => item.toLowerCase());
+*/
+var usernames = ["Umut", "Can"];
+usernames.push("ayse");
+usernames.push(1907); // Argument of type 'number' is not assignable to parameter of type 'string'.
+var surnames = [];
+surnames.push("Kara");
+surnames.push(5); //  Argument of type 'number' is not assignable to parameter of type 'string'.
+surnames.push("25");
+// type decleration 2.yol
+var nums2 = [5, 6];
+console.log(surnames);
--- TypeScript/s01/app.ts
+++ TypeScript/s01/app.ts
@@ -5,13 +5,44 @@ let c = 2;
 
 // b = "5" //Type 'string' is not assignable to type 'number'.
 
-let e: string = "Hello";
-let f = "World!";
+/*
+let e : string = 'Hello'
+let f = "World!"
 
-let g: boolean;
+let g : boolean;
 let h; // any atar otomatik olarak
 
-let x, y, z: string; // tek satırda birden fazla değişkene type belirtemiyoruz, bastakileri any olarak kabul ediyor.
-x = "Merhaba";
-y = 5;
-z = "Hey";
+let x,y,z: string; // tek satırda birden fazla değişkene type belirtemiyoruz, baştakileri any olarak kabul ediyor sonrakine asıl tip ataması yapıyor.
+x="Merhaba"
+y=5
+z = "Oldu"
+z = 5 // error
+*/
+
+//* Arrays
+
+/*
+let nums: number[] = [1, 2, "Hi"];
+
+nums.push("Merhaba");
+nums.push(5);
+nums.push(false);
+
+console.log(nums);
+
+nums.forEach((item) => item.toLowerCase());
+*/
+
+let usernames: string[] = ["Umut", "Can"];
+usernames.push("ayse");
+usernames.push(1907); // Argument of type 'number' is not assignable to parameter of type 'string'.
+
+let surnames: string[] = [];
+surnames.push("Kara");
+surnames.push(5); //  Argument of type 'number' is not assignable to parameter of type 'string'.
+surnames.push("25");
+
+// type decleration 2.yol
+
+let nums2: Array<number> = [5, 6];
+console.log(surnames);
