296a4b26ac77e23ad007389705cc462c9558cead
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-25 12:43:26 +0200

    "Added sweetalert2 dependency to package.json and updated pnpm-lock.yaml accordingly"

---
+++ TypeScript/s03-rct-todoapp/src/helper/notify.ts
@@ -0,0 +1,16 @@
+import Swal from "sweetalert2";
+
+export const enum SweetAlertIcons {
+  WARNING = "warning",
+  ERROR = "error",
+  SUCCESS = "success",
+  INFO = "info",
+}
+
+export const notify = (msg: string, icon: SweetAlertIcons) =>
+  Swal.fire({
+    title: "Todo App!",
+    text: msg,
+    icon: icon,
+    confirmButtonText: "OK",
+  });
