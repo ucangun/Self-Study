dd57dbe5a7ccf888636d49ea8fc1b126368b4467
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-25 10:46:33 +0200

    "Refactored AddTodoComp component to use interface for props type"

--- TypeScript/s03-rct-todoapp/src/components/AddTodoComp.tsx
+++ TypeScript/s03-rct-todoapp/src/components/AddTodoComp.tsx
@@ -2,19 +2,54 @@ import { Box, Button, TextField } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import { useState } from "react";
 
-const AddTodoComp = ({
-  addTodo,
-}: {
-  addTodo: (text: string) => Promise<void>;
-}) => {
-  // const [text, setText] = useState<string>("");
-  const [text, setText] = useState("");
+//! 1. Kullanım yolu props type
+// const AddTodoComp = ({
+//   addTodo,
+// }: {
+//   addTodo: (text: string) => Promise<void>;
+// }) => {
+//   // const [text,setText] = useState<string>("")
+//   const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
+//   const handleClick = () => {
+//     addTodo(text);
+//     setText("");
+//   };
+//   return (
+//     <Box>
+//       <TextField
+//         id="outlined-disabled"
+//         label="New Todo"
+//         variant="outlined"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       />
+//       <Button
+//         variant="contained"
+//         color="success"
+//         disabled={!text.trim()}
+//         onClick={handleClick}
+//         endIcon={<SaveIcon />}
+//       >
+//         Save Todo
+//       </Button>
+//     </Box>
+//   );
+// };
 
+// export default AddTodoComp;
+
+//* 2. Kullanım yolu props type
+interface IAddTodoComp {
+  addTodo: (text: string) => Promise<void>;
+}
+
+const AddTodoComp = ({ addTodo }: IAddTodoComp) => {
+  // const [text,setText] = useState<string>("")
+  const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
   const handleClick = () => {
     addTodo(text);
     setText("");
   };
-
   return (
     <Box>
       <TextField
@@ -28,8 +63,8 @@ const AddTodoComp = ({
         variant="contained"
         color="success"
         disabled={!text.trim()}
-        endIcon={<SaveIcon />}
         onClick={handleClick}
+        endIcon={<SaveIcon />}
       >
         Save Todo
       </Button>
