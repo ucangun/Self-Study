0a1e89d12d6ff575e15c199e84ed74cf768daef3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-25 09:26:35 +0200

    "Added MUI components to AddTodoComp, replaced placeholder div with TextField and Button"

--- TypeScript/s03-rct-todoapp/src/components/AddTodoComp.tsx
+++ TypeScript/s03-rct-todoapp/src/components/AddTodoComp.tsx
@@ -1,5 +1,15 @@
+import { Box, Button, TextField } from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+
 const AddTodoComp = () => {
-  return <div>AddTodoComp</div>;
+  return (
+    <Box>
+      <TextField id="outlined-disabled" label="New Todo" variant="outlined" />
+      <Button variant="contained" color="success" endIcon={<SaveIcon />}>
+        Save Todo
+      </Button>
+    </Box>
+  );
 };
 
 export default AddTodoComp;
