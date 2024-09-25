071b9015b7a5b7b08e746765d798f1973c808aa7
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-25 09:40:24 +0200

    "Added state and effect to Home component to fetch todos from API"

--- TypeScript/s03-rct-todoapp/src/pages/Home.tsx
+++ TypeScript/s03-rct-todoapp/src/pages/Home.tsx
@@ -2,8 +2,35 @@ import { Container } from "@mui/material";
 import Header from "../components/Header";
 import AddTodoComp from "../components/AddTodoComp";
 import TodoList from "../components/TodoList";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const url = "https://634ac3fc5df952851418480f.mockapi.io/api/todos";
+
+interface ITodoType {
+  task: string;
+  isDone: boolean;
+  id: string | number;
+}
 
 const Home = () => {
+  // const [todos, setTodos] = useState([] as ITodoType[]);
+  // const [todos, setTodos] = useState<Array<ITodoType>>([]);
+  const [todos, setTodos] = useState<ITodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <Container>
       <Header />
