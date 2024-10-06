3edf099b1fef0d3f7a8f1f93995dd0a0729eda6f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-06 21:18:15 +0200

    "Removed e2e test file, updated Navbar and Login components with additional data-test attributes and minor formatting changes."

---
+++ Cypress/cypress-starter/cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe("login test", () => {
+  it("login", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-test="loginHeader"]').should("be.visible").contains("recipe");
+    cy.get('[data-test="loginName"]').should("be.visible").type("Anthony");
+    cy.get('[data-test="loginPassword"]')
+      .should("be.visible")
+      .type("Clarusway");
+    cy.get('[data-test="loginSbmt"]').should("be.visible").click();
+    cy.url().should("include", "/home");
+  });
+});
