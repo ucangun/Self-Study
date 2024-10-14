6ed3d34e7931d873ce762ea7f7762802bf2e7684
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-14 11:11:25 +0200

    "Added and commented out code blocks for object destructuring, array destructuring, and OOP concepts in oop.js file."

--- BACKEND/05_OOP_1/oop.js
+++ BACKEND/05_OOP_1/oop.js
@@ -70,6 +70,72 @@ console.log(Car["stopEngine"]());
 //? This keyword
 // icinde bulundugu objeyi ifade eder
 
+/*
+const Car = {
+  brand: "Mercedes",
+  model: "s500",
+  year: 2020,
+  isAutoGear: true,
+  colors: ["black", "blue"],
+  engine: {
+    cylinderCount: 8,
+    hp: 100,
+  },
+  startEngine: function () {
+    return "engine started";
+  },
+  stopEngine: function () {
+    return "engine stopped";
+  },
+  detailFunction: function () {
+    return this;
+  },
+  //! arrow function global scope
+  arrowMethod: () => {
+    return this;
+  },
+};
+*/
+
+/*
+console.log(Car.detailFunction().brand);
+console.log(Car.detailFunction());
+*/
+
+// console.log(Car.arrowMethod());  // anlamsiz
+
+//? Array Destructuring
+
+/*
+
+const sampleArray = ["val1", "val2", "val3", "val4", "val5"];
+
+*/
+
+/*
+const v1 = sampleArray[0];
+const v2 = sampleArray[1];
+const v3 = sampleArray[2];
+const v4 = sampleArray.slice(2, 3);
+*/
+
+// console.log(v4);
+
+/*
+
+// rest Operator => esitligin solunda olacak
+
+const [v1, v2, ...vN] = sampleArray;
+console.log(vN);
+
+// spread operator => esitligin saginda olacak
+
+const newArray = ["valx", "valy", ...sampleArray, "valz"];
+console.log(newArray);
+*/
+
+//? Object Destructuring
+
 const Car = {
   brand: "Mercedes",
   model: "s500",
@@ -95,32 +161,15 @@ const Car = {
   },
 };
 
+// rest
 /*
-console.log(Car.detailFunction().brand);
-console.log(Car.detailFunction());
+const { year, brand, model, ...others } = Car;
+console.log(year, brand, model, others);
+
+console.log("*****************************");
+
+console.log(Car);
 */
 
-// console.log(Car.arrowMethod());  // anlamsiz
-
-//? Array Destructuring
-
-const sampleArray = ["val1", "val2", "val3", "val4", "val5"];
-
-/*
-const v1 = sampleArray[0];
-const v2 = sampleArray[1];
-const v3 = sampleArray[2];
-const v4 = sampleArray.slice(2, 3);
-*/
-
-// console.log(v4);
-
-// rest Operator => esitligin solunda olacak
-
-const [v1, v2, ...vN] = sampleArray;
-console.log(vN);
-
-// spread operator => esitligin saginda olacak
-
-const newArray = ["valx", "valy", ...sampleArray, "valz"];
-console.log(newArray);
+const { year, brand: marka, model, ...others } = Car;
+console.log(year, marka, model, others);
