55e398e82f280026bc616b974cb4a1b2f2c6bf37
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-14 10:42:48 +0200

    "Added and uncommented code blocks for object property access, method calls, and 'this' keyword examples in oop.js"

--- BACKEND/05_OOP_1/oop.js
+++ BACKEND/05_OOP_1/oop.js
@@ -6,6 +6,7 @@
 
 // * Obje isimlendirmede pascalCase / camelCase
 
+/*
 const sampleObject = {
   // property / attribute / field
   propertyName: "value",
@@ -21,12 +22,53 @@ const sampleObject = {
     return "this is method";
   },
 };
+*/
 
-// Obje property cagirma
+//* Obje property cagirma
+
+/*
 console.log(sampleObject.propertyName);
 console.log(sampleObject.methodName());
+*/
 
-// example object
+//* example object
+
+/*
+const Car = {
+  brand: "Mercedes",
+  model: "s500",
+  year: 2020,
+  isAutoGear: true,
+  colors: ["black", "blue"],
+  engine: {
+    cylinderCount: 8,
+    hp: 100,
+  },
+  startEngine: function () {
+    return "engine started";
+  },
+  stopEngine: function () {
+    return "engine stopped";
+  },
+};
+*/
+
+/*
+//* . dot notation
+console.log(Car.brand);
+console.log(Car.colors[1]);
+console.log(Car.startEngine());
+console.log(Car.engine);
+console.log(Car.engine.hp);
+
+//* alternative
+console.log(Car["brand"]);
+console.log(Car["colors"][1]);
+console.log(Car["stopEngine"]());
+*/
+
+//? This keyword
+// icinde bulundugu objeyi ifade eder
 
 const Car = {
   brand: "Mercedes",
@@ -34,10 +76,26 @@ const Car = {
   year: 2020,
   isAutoGear: true,
   colors: ["black", "blue"],
+  engine: {
+    cylinderCount: 8,
+    hp: 100,
+  },
   startEngine: function () {
     return "engine started";
   },
+  stopEngine: function () {
+    return "engine stopped";
+  },
+  detailFunction: function () {
+    return this;
+  },
+  //! arrow function global scope
+  arrowMethod: () => {
+    return this;
+  },
 };
 
-console.log(Car.brand);
-console.log(Car.startEngine());
+console.log(Car.detailFunction().brand);
+console.log(Car.detailFunction());
+
+// console.log(Car.arrowMethod());  // anlamsiz
