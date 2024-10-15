12d50750060e220b2aa9860973a741ab2e25cf7a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-15 10:40:10 +0200

    add polymorphisim example

--- BACKEND/06_OOP_2/oop2.js
+++ BACKEND/06_OOP_2/oop2.js
@@ -13,7 +13,7 @@ const PascalCaseName  = class {
 */
 
 //? Class Decleration (tercih edecegimiz)
-
+/*
 class PascalCaseName {
   //propertyName // undefined deger vermek zorunda degiliz
   propertyName = "value";
@@ -62,7 +62,7 @@ console.log(Mercedes.isRunning);
 // ? INHERITANCE = Miras Alma
 
 // Baska bir sahip olduklari her seyi alir + kendi sahip olduklari
-
+/*
 class Vehicle {
   isActive = false;
   seatCount = 5;
@@ -89,7 +89,7 @@ class Car extends Vehicle {
 
 const Bmw = new Car("BMW", "E30", 2024, "Car");
 console.log(Bmw);
-
+*/
 /*
 class Truck extends Vehicle {
   isRunning = false;
@@ -106,7 +106,7 @@ class Truck extends Vehicle {
   }
 }
 */
-
+/*
 class Accessory extends Car {
   constructor(accessoryName, brand, model, year, vehicleType) {
     super(brand, model, year, vehicleType);
@@ -116,3 +116,52 @@ class Accessory extends Car {
 
 const Seat = new Accessory("Leather", "Mercedes", "s200", 2024, "Car");
 console.log(Seat);
+*/
+
+// ? POLYMORPHISIM
+
+//Override
+//Overload   js desteklemez
+
+class Vehicle {
+  isActive = false;
+  seatCount = 5;
+  hp;
+  constructor(vehicleType) {
+    this.vehicleType = vehicleType;
+  }
+  getDetail() {
+    console.log("this detail about vehicle");
+  }
+}
+
+const newVehicle = new Vehicle("Bus");
+console.log(newVehicle.getDetail());
+
+class Car extends Vehicle {
+  isRunning = false;
+
+  constructor(brand, model, year = 2005, vehicleType) {
+    super(vehicleType); // Üst sınıfın yapıcısını çağırıyoruz ve 'vehicleType' olarak 'car' belirliyoruz
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+  runEngine() {
+    this.isRunning = true;
+    return this.isRunning;
+  }
+  // Overload
+  getDetail() {
+    // parent classtaki fonksiyon override edildi.
+    console.log("this detail about car");
+  }
+  getDetail(x) {
+    // parent classtaki fonksiyon override edildi.
+    console.log(x);
+  }
+}
+
+const Bmw = new Car("BMW", "E30", 2024, "Car");
+console.log(Bmw.getDetail());
+console.log(Bmw.getDetail("test"));
