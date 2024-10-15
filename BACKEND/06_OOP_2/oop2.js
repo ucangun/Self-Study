b8ecd20642e17a69f30a75bd35139c9461d3859e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-15 10:58:18 +0200

    "Added access modifiers (public, private, protected) to Vehicle and Car classes, and updated their constructors and methods accordingly."

--- BACKEND/06_OOP_2/oop2.js
+++ BACKEND/06_OOP_2/oop2.js
@@ -122,7 +122,7 @@ console.log(Seat);
 
 //Override
 //Overload   js desteklemez
-
+/*
 class Vehicle {
   isActive = false;
   seatCount = 5;
@@ -165,3 +165,56 @@ class Car extends Vehicle {
 const Bmw = new Car("BMW", "E30", 2024, "Car");
 console.log(Bmw.getDetail());
 console.log(Bmw.getDetail("test"));
+*/
+
+// ? access modifier
+// ? ENCAPSULATION
+// ? PUBLIC        Parent = YES , Child= YES , Instance = YES
+// ? #PRIVATE      Parent = YES , Child= NO , Instance = NO
+// ? _PROTECTED    Parent = YES , Child= YES , Instance = NO
+
+class Vehicle {
+  publicProp = "this is public property";
+  #privateProp = "this is private property";
+  _protectedProp = "this is protected property";
+
+  isActive = false;
+  seatCount = 5;
+  hp;
+  constructor(vehicleType) {
+    this.vehicleType = vehicleType;
+  }
+  getDetail() {
+    console.log(this.publicProp);
+    console.log(this.#privateProp);
+    console.log(this._protectedProp);
+  }
+}
+
+const newVehicle = new Vehicle("Bus");
+console.log(newVehicle.getDetail());
+
+class Car extends Vehicle {
+  isRunning = false;
+
+  constructor(brand, model, year = 2005, vehicleType) {
+    super(vehicleType); // Üst sınıfın yapıcısını çağırıyoruz ve 'vehicleType' olarak 'car' belirliyoruz
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+  runEngine() {
+    this.isRunning = true;
+    return this.isRunning;
+  }
+  getDetail() {
+    console.log(this.publicProp);
+    // console.log(this.#privateProp);
+    console.log(this._protectedProp);
+  }
+}
+
+const Bmw = new Car("BMW", "E30", 2024, "Car");
+console.log(Bmw.publicProp);
+// console.log(Bmw.#privateProp);
+console.log(Bmw._protectedProp); //!
