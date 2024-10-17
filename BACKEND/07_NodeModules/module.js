50d50364ebfade6ecc0fe30967a554cf1de4c5e0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-17 09:43:13 +0200

    add module example

---
+++ BACKEND/07_NodeModules/module.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// require("./modules/index.js");
+// require("./modules/index");
+// require("./modules");
+
+/* ------------------------------------------------------- */
+
+/*
+ const test = require("./modules/index");
+ test();
+
+ */
+
+/* ------------------------------------------------------- */
+
+/*
+const arrFunc = require("./modules/index");
+
+arrFunc[0]();
+arrFunc[1]();
+arrFunc[2]();
+*/
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
+console.log("this is module js");
---
+++ BACKEND/07_NodeModules/modules/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// console.log("hey this is from module folder ");
+
+/* ------------------------------------------------------- */
+
+/*
+function test() {
+  console.log("this is test function");
+}
+*/
+
+/* ------------------------------------------------------- */
+
+function test1() {
+  console.log("this is test1 function");
+}
+
+function test2() {
+  console.log("this is test2 function");
+}
+
+function test3() {
+  console.log("this is test3 function");
+}
+
+module.exports = [test1, test2, test3];
