308259593d2b627dea5f559b16dd6749aff78058
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-17 10:04:45 +0200

    export and import examples as an array and object

--- BACKEND/07_NodeModules/module.js
+++ BACKEND/07_NodeModules/module.js
@@ -23,6 +23,44 @@ arrFunc[2]();
 */
 
 /* ------------------------------------------------------- */
+
+/*
+
+const [test1, test2, test3] = require("./modules/index");
+
+test1();
+test2();
+test3();
+*/
+
+/* ------------------------------------------------------- */
+
+/*
+const objFunc = require("./modules/index");
+
+objFunc.test1();
+objFunc.test2();
+objFunc.test3();
+*/
+
+/* ------------------------------------------------------- */
+
+/*
+const { test1, test2, test3 } = require("./modules/index");
+
+test1();
+test2();
+test3();
+*/
+
+/* ------------------------------------------------------- */
+
+const Index = require("./modules/index");
+
+Index.test1();
+Index.test2();
+Index.test3();
+
 /* ------------------------------------------------------- */
 
 console.log("this is module js");
--- BACKEND/07_NodeModules/modules/index.js
+++ BACKEND/07_NodeModules/modules/index.js
@@ -24,4 +24,26 @@ function test3() {
   console.log("this is test3 function");
 }
 
-module.exports = [test1, test2, test3];
+/* ------------------------------------------------------- */
+
+exports.test1 = function () {
+  console.log("this is test1 function");
+};
+
+exports.test2 = function () {
+  console.log("this is test2 function");
+};
+
+exports.test3 = function () {
+  console.log("this is test3 function");
+};
+
+/* ------------------------------------------------------- */
+
+// module.exports = [test1, test2, test3];
+
+/* ------------------------------------------------------- */
+
+// module.exports = { test1, test2, test3 };
+
+/* ------------------------------------------------------- */
