de653c61d9cd2408beb4adc13da9a33a83788dcd
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-18 12:11:45 +0200

    "Updated index.js: uncommented and rearranged code blocks, added new HTTP server creation with GET method handling and response headers"

--- BACKEND/08_NodeJsServer/index.js
+++ BACKEND/08_NodeJsServer/index.js
@@ -11,11 +11,11 @@ const HOST = process.env.HOST;
 // HTTP SERVER:
 const http = require("node:http"); // built in module
 
-/* -------------------------------------------- */
+/* -------------------------------------------- *
 
 //http.createServer((req, res) => {});
 
-/*
+
 const app = http.createServer((req, res) => {
   res.end("Hello World response end");
   console.log("Hello from console");
@@ -25,9 +25,9 @@ const app = http.createServer((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
-*/
 
-/* -------------------------------------------- */
+
+/* -------------------------------------------- *
 
 http
   .createServer((req, res) => {
@@ -44,3 +44,40 @@ http
   .listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
   });
+
+
+/* -------------------------------------------- */
+
+http
+  .createServer((req, res) => {
+    /* res.write('<h1>this is write - 1 </h1>')
+     res.write('<h1>this is write - 2 </h1>')
+     res.write('<h1>this is write - 3 </h1>')
+     res.write('<h1>this is write - 4 </h1>') */
+
+    if (req.method == "GET") {
+      // setHeader (single header method)
+      res.setHeader("title", "value");
+
+      // writeHead ( statusCode, statusMessage, {multi headers})
+      res.writeHead(200, {
+        "content-encoding": "utf-8",
+        "multi-headers-exp": "test header value",
+      });
+
+      const obj = {
+        result: true,
+        message: "Hello world!",
+      };
+
+      res.write(JSON.stringify(obj));
+      res.end();
+    } else {
+      res.writeHead(400);
+      res.end("Wrong Method!");
+    }
+  })
+  .listen(PORT, () =>
+    console.log(`Server is running at : http://${HOST}:${PORT}`)
+  );
+/* -------------------------------------------- */
