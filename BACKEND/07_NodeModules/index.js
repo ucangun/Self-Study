0908196d6a56356406c22e3b008b38fdead6660d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-18 10:08:56 +0200

    "Added dotenv configuration and updated start script in package.json"

--- BACKEND/07_NodeModules/index.js
+++ BACKEND/07_NodeModules/index.js
@@ -1,7 +1,21 @@
 "use strict";
+/* -------------------------------------------- *
+                       DOTENV
+/* -------------------------------------------- */
 
-/* ------------------------------------------------------- */
+// for windows
+// "start": "set HOST=localhost&& set PORT=8000&& node index"
 
-console.log("this is index ");
+// for Linux and macOS
+// "start": "HOST=localhost PORT=8000 node index"
+
+console.log("this is indexjs");
+
+/* -------------------------------------------- */
+
+require("dotenv").config();
 
 console.log(process.env.PORT);
+console.log(process.env.HOST);
+
+/* -------------------------------------------- */
