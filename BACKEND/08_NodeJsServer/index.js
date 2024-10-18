e6fb33fe286b99c1919fd98ab591923db037ec67
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-18 11:37:27 +0200

    "Added new HTTP server implementation with routing for home and about pages, and commented out previous implementation."

--- BACKEND/08_NodeJsServer/index.js
+++ BACKEND/08_NodeJsServer/index.js
@@ -15,6 +15,7 @@ const http = require("node:http"); // built in module
 
 //http.createServer((req, res) => {});
 
+/*
 const app = http.createServer((req, res) => {
   res.end("Hello World response end");
   console.log("Hello from console");
@@ -24,5 +25,22 @@ const app = http.createServer((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
+*/
 
 /* -------------------------------------------- */
+
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+
+    if (req.url == "/") {
+      res.end("<h1>Home Page</h1>");
+    } else if (req.url == "/about") {
+      res.end("<h1>About Page</h1>");
+    } else {
+      res.end("<h1>This is any Page</h1>");
+    }
+  })
+  .listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+  });
