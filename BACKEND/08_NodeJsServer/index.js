9a13354d66f115995be8344db831192c2493385f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-18 11:04:22 +0200

    "Added HTTP server creation and listening code using node:http module, replaced console log with server response and startup message."

--- BACKEND/08_NodeJsServer/index.js
+++ BACKEND/08_NodeJsServer/index.js
@@ -8,4 +8,21 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
-console.log("Hello World", PORT, HOST);
+// HTTP SERVER:
+const http = require("node:http"); // built in module
+
+/* -------------------------------------------- */
+
+//http.createServer((req, res) => {});
+
+const app = http.createServer((req, res) => {
+  res.end("Hello World response end");
+  console.log("Hello from console");
+});
+
+// DEFAULT server domain (local domain) => localhost : 127.0.0.1
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
+
+/* -------------------------------------------- */
