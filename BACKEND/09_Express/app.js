a41d03835d09253e0e5e032fabb3410c3a15b2c6
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-21 09:59:01 +0200

    "Added dotenv package to load environment variables from .env file and updated server listening configuration to use PORT and HOST variables"

--- BACKEND/09_Express/app.js
+++ BACKEND/09_Express/app.js
@@ -11,8 +11,17 @@
 //? Express server start
 
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config(); // .env deki tüm degiskenler process env ye alinir
+
+// console.log(process.env);
+
 const app = express(); // express üzerine server calistir
 
-// app.listen (8000)
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "127.0.0.1";
 
-app.listen(8000, () => console.log("Server is running"));
+// app.listen (8000)
+app.listen(PORT, () =>
+  console.log(`Server is running on http://${HOST}:${PORT}`)
+);
