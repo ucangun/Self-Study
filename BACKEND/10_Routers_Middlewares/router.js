6711bd346f699f52c40fe377944d71ba259c6a63
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-22 09:41:46 +0200

    "Refactor router.js: replaced single app.get route with express.Router instance and added multiple routes (currently commented out)"

--- BACKEND/10_Routers_Middlewares/router.js
+++ BACKEND/10_Routers_Middlewares/router.js
@@ -8,8 +8,26 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const router = express.Router();
+
+app.use(router);
+
+/*
+router.get("/", (req, res) => {
+  res.send({
+    message: "GET",
+  });
 });
+router.get("/user", (req, res) => {
+  res.send({
+    message: "GET for user path",
+  });
+});
+router.post("/", (req, res) => {
+  res.send({
+    message: "POST",
+  });
+});
+*/
 
 app.listen(PORT, () => console.log(`Server running on http://${HOST}:${PORT}`));
