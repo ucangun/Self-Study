42441c8537c4b8262f70471cc6855e8b8ebd3a65
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-22 10:41:03 +0200

    "Added new middleware function to check username and updated route handler"

--- BACKEND/10_Routers_Middlewares/middleware.js
+++ BACKEND/10_Routers_Middlewares/middleware.js
@@ -11,6 +11,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 
+/*
 app.get("/", (req, res, next) => {
   console.log("Middleware 1 calisti");
   next();
@@ -22,6 +23,24 @@ app.get("/", (req, res, next) => {
 app.get("/", (req, res, next) => {
   res.send({
     message: "Hello World",
+  });
+});
+*/
+
+app.get("/", (req, res, next) => {
+  console.log("Middleware 1 calisti");
+  if (req.query.username == "clarusway") {
+    next();
+  } else {
+    res.send({
+      message: "username not exist",
+    });
+  }
+});
+
+app.get("/", (req, res, next) => {
+  res.send({
+    message: "Welcome Clarusway",
   });
 });
 
