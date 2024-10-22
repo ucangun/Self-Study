66420e53aa4ca12c4ee699b0199c6463c363e534
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-22 10:33:17 +0200

    "Changed app.all to three separate app.get middlewares with console logs and updated response message"

--- BACKEND/10_Routers_Middlewares/middleware.js
+++ BACKEND/10_Routers_Middlewares/middleware.js
@@ -11,9 +11,17 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 
-app.all("/", (req, res) => {
+app.get("/", (req, res, next) => {
+  console.log("Middleware 1 calisti");
+  next();
+});
+app.get("/", (req, res, next) => {
+  console.log("Middleware 2 calisti");
+  next();
+});
+app.get("/", (req, res, next) => {
   res.send({
-    message: "Middleware",
+    message: "Hello World",
   });
 });
 
