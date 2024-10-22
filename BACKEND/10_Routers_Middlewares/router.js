4317ab6bf3f8c6e8a6b4c462f44dc51d24f1ea47
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-22 09:57:36 +0200

    "Refactor router.js: replaced express.Router() with require('./routers/index') and removed inline route definitions"

--- BACKEND/10_Routers_Middlewares/router.js
+++ BACKEND/10_Routers_Middlewares/router.js
@@ -8,26 +8,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 
-const router = express.Router();
+const router = require("./routers/index");
 
 app.use(router);
-
-/*
-router.get("/", (req, res) => {
-  res.send({
-    message: "GET",
-  });
-});
-router.get("/user", (req, res) => {
-  res.send({
-    message: "GET for user path",
-  });
-});
-router.post("/", (req, res) => {
-  res.send({
-    message: "POST",
-  });
-});
-*/
 
 app.listen(PORT, () => console.log(`Server running on http://${HOST}:${PORT}`));
---
+++ BACKEND/10_Routers_Middlewares/routers/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const router = require("express").Router();
+
+//?  router.route()
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.send("Get");
+  })
+  .post((req, res) => {
+    res.send("Post");
+  })
+  .put((req, res) => {
+    res.send("Put");
+  });
+
+module.exports = router;
