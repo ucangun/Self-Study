6c4932366b106b58249d9e3680770f871e3bc83f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-24 09:28:55 +0200

    "Added GET /user/:id endpoint to error.js"

--- BACKEND/11_Error_Management/error.js
+++ BACKEND/11_Error_Management/error.js
@@ -12,7 +12,16 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-
 const PORT = process.env.PORT || 8000;
+
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send({
+    error: false,
+    message: "User found",
+    id,
+  });
+});
 
 app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
