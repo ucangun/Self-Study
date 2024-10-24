e2d5e264a1b26d3e8975d094ab4ccedfddd4049b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-24 09:21:37 +0200

    "Initial setup: Initialized project with Express and dotenv, created .gitignore, and set up basic server"

---
+++ BACKEND/11_Error_Management/error.js
@@ -0,0 +1,18 @@
+/* ------------------------------------------------
+            Error Handling in Node.js
+---------------------------------------------------*/
+
+/*
+npm init-y 
+npm i express dotenv
+cat > .gitignore (kopyalanan verileri terminale yapistir ardindan ctrl + c ile cikis yap )
+*/
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
