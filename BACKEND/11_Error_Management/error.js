8cdd800cc7bcc87230bffed4fa712db7013b2d80
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-24 10:01:05 +0200

    "Added input validation and error handling to /user/:id route"

--- BACKEND/11_Error_Management/error.js
+++ BACKEND/11_Error_Management/error.js
@@ -14,14 +14,39 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
+/* ------------------------------------------------*/
+
 app.get("/user/:id", (req, res) => {
   const { id } = req.params;
 
-  res.send({
-    error: false,
-    message: "User found",
-    id,
-  });
+  if (isNaN(id)) {
+    // res.errorStatusCode = 400;
+    throw new Error("Invalid ID: ID should be a number", {
+      cause: "params.id",
+    });
+  } else {
+    res.send({
+      error: false,
+      message: "User found",
+      id,
+    });
+  }
 });
+
+/*---------------------------------------------------*/
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res?.errorStatusCode ?? 500;
+  // console.error(error.message);
+  res.status(res.errorStatusCode).send({
+    error: true,
+    message: error.message,
+    cause: error.cause,
+  });
+};
+
+app.use(errorHandler);
+
+/*---------------------------------------------------*/
 
 app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
