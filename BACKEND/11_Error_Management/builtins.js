ba7c9eb41ca50a70539c19da5e85af03c48f2a69
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-24 11:43:33 +0200

    Implement basic Express.js server with dynamic route handling

---
+++ BACKEND/11_Error_Management/builtins.js
@@ -0,0 +1,24 @@
+"use strict";
+/* ------------------------------------------------
+            Express.js Built in Middlewares
+---------------------------------------------------*/
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+app.all("/:id", (req, res) => {
+  const { params, query, body, headers } = req;
+
+  res.send({
+    error: false,
+    params,
+    query,
+    body,
+    headers,
+  });
+});
+
+app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`));
