1f09f64d203d56f31bac5752dc532761956986db
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-27 15:25:01 +0100

    "Updated db.sqlite3 and refactored index.js to add CRUD operations for Todo"

--- BACKEND/12_Todo_Sequelize_1/index.js
+++ BACKEND/12_Todo_Sequelize_1/index.js
@@ -144,7 +144,7 @@ router.delete("/todo/:id", async (req, res) => {
   const deleted = await Todo.destroy({ where: { id } });
 
   if (deleted) {
-    return res.status(204).send(); // Başarıyla silindiyse yanıt gövdesi yok
+    return res.status(204).send();
   } else {
     res.status(404).send({
       error: true,
@@ -152,6 +152,59 @@ router.delete("/todo/:id", async (req, res) => {
     });
   }
 });
+
+/* ------------------------------------------------------- *
+router
+  .route("/todo")
+  .get(async (req, res) => {
+    const result = await Todo.findAndCountAll();
+    res.status(200).send({
+      error: false,
+      data: result,
+    });
+  })
+  .post(async (req, res) => {
+    const result = await Todo.create(req.body);
+    res.status(201).send({
+      error: false,
+      data: result,
+    });
+  });
+
+router
+  .route("/todo/:id")
+  .put(async (req, res) => {
+    const id = req.params.id;
+    const [updated] = await Todo.update(req.body, { where: { id } });
+
+    if (updated) {
+      const updatedTodo = await Todo.findOne({ where: { id } });
+      res.status(200).send({
+        error: false,
+        message: "Todo updated successfully",
+        data: updatedTodo,
+      });
+    } else {
+      res.status(404).send({
+        error: true,
+        message: "Todo not found",
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    const deleted = await Todo.destroy({ where: { id } });
+
+    if (deleted) {
+      return res.status(204).send();
+    } else {
+      res.status(404).send({
+        error: true,
+        message: "Todo not found",
+      });
+    }
+  });
+/* ------------------------------------------------------- */
 
 app.use(router);
 
