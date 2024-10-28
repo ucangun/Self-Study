a1949c7c1d7ebde4af3cbd3c05950c3a3d280348
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-28 10:45:53 +0100

    "Moved Sequelize configuration and Todo model from index.js to src/models/todo.js"

--- BACKEND/12_Todo_Sequelize_1/index.js
+++ BACKEND/12_Todo_Sequelize_1/index.js
@@ -9,92 +9,13 @@ const app = express();
 require("express-async-errors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-const DB_PATH = process.env.DB_PATH || "./db.sqlite3";
-const DB_NAME = process.env.DB_NAME || "sqlite";
+
+
 
 /* ------------------------------------------------------- */
 
 // Accept json data:
 app.use(express.json());
-
-/*
-app.all("/", (req, res) => {
-  res.send("WELCOME TO TODO API");
-});
-*/
-
-/* ------------------------------------------------------- */
-
-// Sequlize
-const { Sequelize, DataTypes } = require("sequelize");
-
-// Creating new instance
-const sequelize = new Sequelize(`${DB_NAME}:${DB_PATH}`); // define your db and the path
-
-//* Crerating Model
-// sequelize.define("modelName", { fields});
-
-const Todo = sequelize.define("todos", {
-  /*
-  id: {
-    // this att. created auto
-    type: DataTypes.INTEGER,
-    allowNull: false, // default : true
-    unique: true, // default : false
-    comment: "this is comment",
-    primaryKey: true, // default : false
-    autoIncrement: true, // default : false
-    field: "custom_name",
-    defaultValue: 0, // default : null
-  },*/
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-
-  description: DataTypes.TEXT, //shorthand using
-
-  priority: {
-    // -1: low , 0: normal , 1:high
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: 0,
-  },
-
-  isDone: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-
-  // No need to define createdAt and updatedAt (auto)
-});
-
-/* ------------------------------------------------------- */
-
-// Sync - Just execute once to update model then comment again
-
-//sequelize.sync(); // executing model
-//sequelize.sync({ force: true }); // drop table and create new
-// sequelize.sync({ alter: true }); // to backup & drop table & create new table from backup
-// sequelize.sync({ alter: true }); // to backup & drop table & create new table from backup
-
-/* ------------------------------------------------------- */
-
-// Connecting to DB
-
-const testConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-
-testConnection();
-
-/* ------------------------------------------------------- */
 
 //* Routes
 
