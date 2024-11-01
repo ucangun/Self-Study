2f32f979df2ae7b2d450f3d4fe32c0c6f6cc7217
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-01 09:46:26 +0100

    initial commit

---
+++ BACKEND/14_BlogApi_Mongoose/index.js
@@ -0,0 +1,30 @@
+"use strickt";
+
+/*****************************************************
+
+    EXPRESS.JS - BLOG Project with Mongoose 
+
+/****************************************************/
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+
+// Middlewares:
+app.use(express.json());
+require("express-async-errors");
+
+app.all("/", (req, res) => {
+  res.send("WELCOME TO BLOG API");
+});
+
+// Catch Errors:
+app.use(require("./src/errorHandler"));
+
+/* ------------------------------------------------------- */
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
