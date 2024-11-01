aae1eaf007ca224e5323e10edbe3254727acc263
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-01 10:43:12 +0100

    template for mongoose Schema

---
+++ BACKEND/14_BlogApi_Mongoose/src/models/blog.js
@@ -0,0 +1,44 @@
+/******************************************************
+
+    EXPRESS.JS - BLOG Project with Mongoose 
+
+/*************************************************** */
+
+const mongoose = require("mongoose");
+
+// const schemaName = new mongoose.Schema({ ...fields }, { settings });
+
+/*************************************************** /
+
+const schemaName = new mongoose.Schema(
+  {
+    // _id: ObjectId,
+    fieldName: {
+      type: Number,
+      default: null, // if value not send
+      trim: true,
+      unique: true,
+      // required: true,
+      required: [true, "This is required fieldname"],
+      index: true, // to access data faster
+      // enum: ["1", "2", "3"],
+      enum: [["1", "2", "3"], "This is enum error msg"],
+      // validate: (data) => true or false
+      validate: [
+        (data) => {
+          return true;
+        },
+        "This is validate error msg",
+      ],
+      get: (data) => data, // auto runs - to get data from db
+      set: (data) => data, // auto runs - to save the data to db
+    },
+    fieldName: String, // shorthand
+  },
+  {
+    collection: "tableName",
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+/*************************************************** */
