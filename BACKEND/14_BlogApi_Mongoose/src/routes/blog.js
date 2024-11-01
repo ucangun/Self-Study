6acd8c6c0b42b6576b4c02c8aae0ab88ccd2d345
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-01 12:04:45 +0100

    "Added GET endpoints for blog categories"

--- BACKEND/14_BlogApi_Mongoose/src/routes/blog.js
+++ BACKEND/14_BlogApi_Mongoose/src/routes/blog.js
@@ -11,6 +11,8 @@ const router = express.Router();
 
 const { blogCategory, blogPost } = require("../controllers/blog");
 
-router.route("/category").post(blogCategory.create);
+router.route("/category").get(blogCategory.list).post(blogCategory.create);
+
+router.route("/category/:id").get(blogCategory.read);
 
 module.exports = router;
