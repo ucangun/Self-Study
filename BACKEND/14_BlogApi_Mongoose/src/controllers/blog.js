0c759c297e89f753f29ed07ab1a2683553c2e8ac
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-04 10:37:08 +0100

    "Added blog post controller with list, create, read, update, and delete methods"

--- BACKEND/14_BlogApi_Mongoose/src/controllers/blog.js
+++ BACKEND/14_BlogApi_Mongoose/src/controllers/blog.js
@@ -71,4 +71,53 @@ module.exports.blogCategory = {
 
 // Blog Post Controller
 
-module.exports.blogPost = {};
+module.exports.blogPost = {
+  list: async (req, res) => {
+    const result = await BlogPost.find();
+
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
+
+  create: async (req, res) => {
+    const result = await BlogPost.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
+    const result = await BlogPost.findById(req.params.categoryId);
+
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
+
+  update: async (req, res) => {
+    const { postId } = req.params;
+
+    const updatedData = await BlogPost.findByIdAndUpdate(postId, req.body, {
+      new: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      updatedData,
+    });
+  },
+
+  delete: async (req, res) => {
+    const { deletedCount } = await BlogPost.deleteOne({
+      _id: req.params.categoryId,
+    });
+
+    if (deletedCount) res.sendStatus(204);
+    else throw new Error("Something went wrong!");
+  },
+};
