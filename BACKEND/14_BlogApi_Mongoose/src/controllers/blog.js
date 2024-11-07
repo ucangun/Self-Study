f38c7a5b32b716c7d150fc32cb1c9aabee04050d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-07 12:10:41 +0100

    "Updated limit and pagination logic in blogPost controller"

--- BACKEND/14_BlogApi_Mongoose/src/controllers/blog.js
+++ BACKEND/14_BlogApi_Mongoose/src/controllers/blog.js
@@ -100,14 +100,17 @@ module.exports.blogPost = {
 
     //* LIMIT
 
-    const limit = req.query?.limit || 20;
-    console.log(limit);
+    const limit = Number(req.query?.limit);
+    limit = limit > 0 ? limit : 20;
 
     //* PAGINATION
     // URL?page=2&limit=10
 
-    const page = Number(req.query.page) || 1;
-    const skip = (page - 1) * limit;
+    const page = Number(req.query.page);
+    page = page > 0 ? page : 1;
+
+    let skip = Number(req.query?.skip);
+    skip = skip > 0 ? skip : (page - 1) * limit;
 
     console.log("line 101 => ", req.query);
 
