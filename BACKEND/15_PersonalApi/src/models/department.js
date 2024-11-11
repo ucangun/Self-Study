18205755c043b7679a740b62024435c039415557
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-11 09:41:16 +0100

    "Added Mongoose schema and model for Department"

--- BACKEND/15_PersonalApi/src/models/department.js
+++ BACKEND/15_PersonalApi/src/models/department.js
@@ -1,6 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const { model, Schema } = require("mongoose");
+
+const DepartmentSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    collection: "departments",
+    timestamps: true,
+  }
+);
+
+module.exports = model("Department", DepartmentSchema);
 
 /* ------------------------------------------------------- */
