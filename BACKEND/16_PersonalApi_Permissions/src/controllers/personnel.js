4003d79d66a1c72b0870dd343317e3019d3a07b1
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-12 12:50:44 +0100

    "Updated department and personnel controllers with minor code formatting changes and added todo comments for future lead management and multi-delete features."

--- BACKEND/16_PersonalApi_Permissions/src/controllers/personnel.js
+++ BACKEND/16_PersonalApi_Permissions/src/controllers/personnel.js
@@ -1,57 +1,57 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-const Personnel = require('../models/personnel')
-
+const Personnel = require("../models/personnel");
 
 module.exports = {
-    list: async (req, res) => {
-        const result = await res.getModelList(Personnel);
+  list: async (req, res) => {
+    const result = await res.getModelList(Personnel);
 
-        res.status(200).send({
-            error: false,
-            detail: await res.getModelListDetails(Personnel),
-            result,
-        });
-    },
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Personnel),
+      result,
+    });
+  },
 
-    create: async (req, res) => {
-        const result = await Personnel.create(req.body);
-        res.status(201).send({
-            error: false,
-            result,
-        });
-    },
+  create: async (req, res) => {
+    const result = await Personnel.create(req.body);
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
 
-    read: async (req, res) => {
-        const result = await Personnel.findOne({ _id: req.params.id });
+  read: async (req, res) => {
+    const result = await Personnel.findOne({ _id: req.params.id });
 
-        res.status(200).send({
-            error: false,
-            result,
-        });
-    },
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
 
-    update: async (req, res) => {
-        const result = await Personnel.updateOne({ _id: req.params.id }, req.body, {
-            runValidators: true,
-        });
+  update: async (req, res) => {
+    const result = await Personnel.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
 
-        res.status(202).send({
-            error: false,
-            result,
-            new: await Personnel.findOne({ _id: req.params.id }),
-        });
-    },
+    res.status(202).send({
+      error: false,
+      result,
+      new: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
 
-    delete: async (req, res) => {
-        const result = await Personnel.deleteOne({ _id: req.params.id });
-        res.status(result.deletedCount ? 204 : 404).send({
-            error: !result.deletedCount,
-            result,
-        });
-    },
+  delete: async (req, res) => {
+    const result = await Personnel.deleteOne({ _id: req.params.id });
+    res.status(result.deletedCount ? 204 : 404).send({
+      error: !result.deletedCount,
+      result,
+    });
+  },
 
+  // multidelete
 };
