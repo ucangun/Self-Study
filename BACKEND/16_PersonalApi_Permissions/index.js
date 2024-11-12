c254df2c9d52cb5b2839d16a2dae7bc942d09101
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-12 09:49:49 +0100

    "Added token route and imported in index.js"

--- BACKEND/16_PersonalApi_Permissions/index.js
+++ BACKEND/16_PersonalApi_Permissions/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
@@ -15,64 +15,61 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 // Middlewares:
-app.use(express.json())
-require('express-async-errors')
+app.use(express.json());
+require("express-async-errors");
 
 // Session-Cookies
-const session = require('cookie-session');
-app.use(session({
+const session = require("cookie-session");
+app.use(
+  session({
     secret: process.env.SECRET_KEY,
     // cookie: {
     //     secure: true, // default false
     //     httpOnly: false // default false
     // }
-}))
-
-
+  })
+);
 
 // Query Handler
-app.use(require('./src/middlewares/queryHandler'))
+app.use(require("./src/middlewares/queryHandler"));
 
 // DB connection:
-require('./src/configs/dbConnection')
-
+require("./src/configs/dbConnection");
 
 /* ------------------------------------------------------- */
 // Routes:
 
-// main 
-app.all('/', (req, res) => {
+// main
+app.all("/", (req, res) => {
+  res.send({
+    message: "WELCOME TO PERSONNEL API",
+    isLogin: req.session.id ? true : false,
+    session: req.session,
+  });
+});
 
-    res.send({
-        message: 'WELCOME TO PERSONNEL API',
-        isLogin: req.session.id ? true : false,
-        session: req.session
-    })
-})
-
-// department
-app.use('/departments', require('./src/routes/department'))
-// personnel
-app.use('/personnels', require('./src/routes/personnel'))
 // auth
-app.use('/auth', require('./src/routes/auth'))
+app.use("/auth", require("./src/routes/auth"));
+// tokens
+app.use("/tokens", require("./src/routes/token"));
+// department
+app.use("/departments", require("./src/routes/department"));
+// personnel
+app.use("/personnels", require("./src/routes/personnel"));
 
 // Not Found
-app.use('*', (req, res) => {
-
-    res.status(404).send({
-        error: true,
-        message: "This route is not found !"
-    })
-})
-
-
+app.use("*", (req, res) => {
+  res.status(404).send({
+    error: true,
+    message: "This route is not found !",
+  });
+});
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 //! Syncronization : Run it only once.
