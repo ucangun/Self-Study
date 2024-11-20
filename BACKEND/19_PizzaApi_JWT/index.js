b45ada48279005d7007d2c7577b1ef67d91adc61
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-20 12:25:24 +0100

    "Updated index.js: added semicolons, changed quotes, and reformatted code"

--- BACKEND/19_PizzaApi_JWT/index.js
+++ BACKEND/19_PizzaApi_JWT/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
@@ -11,25 +11,25 @@
     $ nodemon
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* // Cross-origin resource sharing (CORS): $ npm i cors
 const cors = require('cors')
@@ -45,56 +45,43 @@ app.use(cors()) */
 // Middlewares:
 
 // Parse JSON:
-app.use(express.json())
+app.use(express.json());
 
 // Logger:
-app.use(require('./src/middlewares/logger'))
+app.use(require("./src/middlewares/logger"));
 
 // Auhentication: (JWT & Simple Token)
-app.use(require('./src/middlewares/authentication'))
+app.use(require("./src/middlewares/authentication"));
 
 // findSearchSortPage / res.getModelList:
-app.use(require('./src/middlewares/queryHandler'))
+app.use(require("./src/middlewares/queryHandler"));
 
 /* ------------------------------------------------------- */
 // Routes:
 
-// HomePath:
-app.all('/', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to PIZZA API',
-        docs: {
-            swagger: "/documents/swagger",
-            redoc: "/documents/redoc",
-            json: "/documents/json",
-        },
-        user: req.user,
-    })
-})
+app.use("/", require("./src/routes"));
 
-// Auth:
-app.use('/auth', require('./src/routes/auth'))
-// Tokens:
-app.use('/tokens', require('./src/routes/token'))
-// Users:
-app.use('/users', require('./src/routes/user'))
-// Toppings:
-app.use('/toppings', require('./src/routes/topping'))
-// Pizza:
-app.use('/pizzas', require('./src/routes/pizza'))
-// Order:
-app.use('/orders', require('./src/routes/order'))
-// Documents:
-app.use('/documents', require('./src/routes/document'))
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PIZZA API",
+    docs: {
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
+    },
+    user: req.user,
+  });
+});
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
