ac4a8aff397da9ff164525fa35634002f0ffe027
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-20 11:07:58 +0100

    "Updated authentication middleware to handle both 'Token' and 'Bearer' authorization headers, and added JWT verification for 'Bearer' tokens."

--- BACKEND/19_PizzaApi_JWT/src/middlewares/authentication.js
+++ BACKEND/19_PizzaApi_JWT/src/middlewares/authentication.js
@@ -1,20 +1,30 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 
-const Token = require('../models/token')
+const Token = require("../models/token");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
+  req.user = null;
 
-    const auth = req.headers?.authorization // Token ...tokenKey...
-    const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...']
+  const auth = req.headers?.authorization; // Token ...tokenKey... || Bearer ...accessToken
 
-    if (tokenKey && tokenKey[0] == 'Token') {
-        const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('userId')
-        
-        req.user = tokenData ? tokenData.userId : false
-    }
+  const tokenKey = auth ? auth.split(" ") : null; // ['Bearer', '...tokenKey...']
 
-    next()
-}
+  if (tokenKey && tokenKey[0] === "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+      "userId"
+    );
+    req.user = tokenData ? tokenData.userId : false;
+  } else if (tokenKey && tokenKey[0] === "Bearer") {
+    jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (err, accessData) => {
+      //   console.log(err);
+      //   console.log(accessData);
+      req.user = accessData ? accessData : null;
+    });
+  }
+
+  next();
+};
