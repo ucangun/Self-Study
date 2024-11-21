3c3f97c328f8bf523dbfc9e0cd967c1c50567c94
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-21 11:26:40 +0100

    "Added Multer middleware for file uploads, updated pizza route to use Multer, and modified package.json and package-lock.json files to include Multer dependency."

--- BACKEND/20_PizzaApi_Email_Upload/src/routes/pizza.js
+++ BACKEND/20_PizzaApi_Email_Upload/src/routes/pizza.js
@@ -1,18 +1,42 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
+const {
+  list,
+  create,
+  read,
+  update,
+  deletePizza,
+} = require("../controllers/pizza");
 
-const { list, create, read, update, deletePizza } = require('../controllers/pizza')
+// UPLOAD (Multer Middleware)
+// npm i multer
+
+const multer = require("multer");
+const upload = multer({
+  //  dest: "./uploads/"
+  storage: multer.diskStorage({
+    destination: "./uploads",
+    filename: (req, file, returnCallback) => {
+      console.log("file", file);
+      // returnCallback(error ,filename) syntax
+      returnCallback(null, file.originalname);
+    },
+  }),
+});
 
 // URL: /pizzas
 
-router.route('/').get(list).post(create)
+router.route("/").get(list).post(upload.single("image"), create);
+// .post(upload.single(), create);
+// .post(upload.array("image"), create)
+// .post(upload.any(), create)
 
-router.route('/:id').get(read).put(update).patch(update).delete(deletePizza)
+router.route("/:id").get(read).put(update).patch(update).delete(deletePizza);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
