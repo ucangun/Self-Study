7eec70664b9fb9863d8f5023edb707fd4b90a31e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-21 09:45:44 +0100

    "Updated index.js file with minor syntax changes, added nodemailer package and email functionality, and updated package-lock.json and package.json files accordingly."

--- BACKEND/20_PizzaApi_Email_Upload/index.js
+++ BACKEND/20_PizzaApi_Email_Upload/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
@@ -11,25 +11,25 @@
     $ nodemon
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* // Cross-origin resource sharing (CORS): $ npm i cors
 const cors = require('cors')
@@ -45,45 +45,98 @@ app.use(cors()) */
 // Middlewares:
 
 // Parse JSON:
-app.use(express.json())
+app.use(express.json());
 
 // Logger:
-app.use(require('./src/middlewares/logger'))
+app.use(require("./src/middlewares/logger"));
 
 // Auhentication: (JWT & Simple Token)
-app.use(require('./src/middlewares/authentication'))
+app.use(require("./src/middlewares/authentication"));
 
 // findSearchSortPage / res.getModelList:
-app.use(require('./src/middlewares/queryHandler'))
+app.use(require("./src/middlewares/queryHandler"));
+
+/* ------------------------------------------------------- */
+// E - MAIL:
+// npm i nodemailer
+
+const nodemailer = require("nodemailer");
+
+// Create Test Account
+
+// nodemailer.createTestAccount().then((data) => console.log(data));
+
+// /*
+// {
+//     user: 'wd45z7xxh7cep2kx@ethereal.email',
+//     pass: '4mkvFJwb4sEZa8BSxv',
+//     smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+//     imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+//     pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
+//     web: 'https://ethereal.email',
+//     mxEnabled: false
+//   }
+// */
+
+// // Connect to MailServer / SMTP
+// const transporter = nodemailer.createTransport({
+//   // SMTP
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: "wd45z7xxh7cep2kx@ethereal.email",
+//     pass: "4mkvFJwb4sEZa8BSxv",
+//   },
+// });
+
+// // console.log(transporter);
+
+// const mailOptions = {
+//   from: "wd45z7xxh7cep2kx@ethereal.email",
+//   to: "ucangun76@gmail.com",
+//   subject: "Hello World!",
+//   text: "Hello there , how are you ?",
+//   html: "<p><b>Hello There</b> <br> How are you ? </p>",
+// };
+
+// transporter.sendMail(mailOptions, (err, info) => {
+//   err
+//     ? console.error("Hata oluştu:", err)
+//     : console.log(
+//         "E-posta gönderildi:",
+//         info.messageId,
+//         "\nE-posta önizleme URL:",
+//         nodemailer.getTestMessageUrl(info)
+//       );
+// });
 
 /* ------------------------------------------------------- */
 // Routes:
 
 // All Routes
-app.use("/", require('./src/routes'))
-
+app.use("/", require("./src/routes"));
 
 // HomePath:
-app.all('/', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to PIZZA API',
-        docs: {
-            swagger: "/documents/swagger",
-            redoc: "/documents/redoc",
-            json: "/documents/json",
-        },
-    })
-})
-
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PIZZA API",
+    docs: {
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
+    },
+  });
+});
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
