973536aca82e11261ce66d48074366e87269a6ed
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-29 10:28:41 +0100

    "Added car routes and permissions middleware to car.js"

--- BACKEND/22-RentACar/src/routes/car.js
+++ BACKEND/22-RentACar/src/routes/car.js
@@ -3,6 +3,18 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const router = require("express").Router();
+const car = require("../controllers/car");
+const permissions = require("../middlewares/permissions");
 /* ------------------------------------------------------- */
 // routes/car:
+
+router.route("/").get(car.list).post(permissions.isStaffOrisAdmin, car.create);
+
+router
+  .route("/:id")
+  .get(car.read)
+  .put(permissions.isStaffOrisAdmin, car.update)
+  .patch(permissions.isStaffOrisAdmin, car.update)
+  .delete(permissions.isAdmin, car.delete);
+
 module.exports = router;
