1644b9f5339764da226538c4e7f0b083a55fe2b9
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-29 10:33:24 +0100

    "Added fields to ReservationSchema in reservation.js model"

--- BACKEND/22-RentACar/src/models/reservation.js
+++ BACKEND/22-RentACar/src/models/reservation.js
@@ -19,11 +19,50 @@ const { mongoose } = require("../configs/dbConnection");
 
 // Reservation Model:
 const ReservationSchema = new mongoose.Schema(
-  {},
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    carId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+    //begin, finish,reservationStart ve reservationEnd
+    startDate: {
+      type: Date,
+      required: true,
+    },
+
+    endDate: {
+      type: Date,
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    createdId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    updatedId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
   {
     collection: "reservations",
     timestamps: true,
-  },
+  }
 );
 
 // Export:
