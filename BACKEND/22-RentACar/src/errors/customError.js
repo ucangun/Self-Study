905a48423e70b32027d2c8a395156842699949e4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-29 10:54:15 +0100

    "Updated CustomError class to accept statusCode and modified errorHandler middleware to use error statusCode"

--- BACKEND/22-RentACar/src/errors/customError.js
+++ BACKEND/22-RentACar/src/errors/customError.js
@@ -1,3 +1,8 @@
-class CustomError extends Error {}
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode || 500;
+  }
+}
 
 module.exports = CustomError;
--- BACKEND/22-RentACar/src/middlewares/errorHandler.js
+++ BACKEND/22-RentACar/src/middlewares/errorHandler.js
@@ -5,7 +5,7 @@
 // app.use(errorHandler):
 
 module.exports = (err, req, res, next) => {
-  return res.status(res?.errorStatusCode || 500).send({
+  return res.status(err?.statusCode || res?.errorStatusCode || 500).send({
     error: true,
     message: err.message,
     cause: err.cause,
