977d678cc9135a2adbd9d5ac818c2c4ec4477fdf
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-02 13:52:30 +0100

    "add todo server side files"

---
+++ BACKEND/12_FS_Todo_App/server/index.js
@@ -0,0 +1,44 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Todo API
+------------------------------------------------------- */
+
+const express = require('express')
+const app = express()
+
+
+//* Required Modules:
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+
+require('express-async-errors')
+
+//* Configrations:
+
+require('./src/configs/dbConnection')()
+
+//* Middlewares:
+
+app.use(express.json())
+
+
+//
+
+
+//* Routes:
+
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to TODO API',
+    })
+})
+
+
+app.use('/todo', require('./src/routers/todo'))
+
+
+
+app.use(require('./src/middlewares/errorHandler'))
+
+app.listen(PORT, () => console.log('Listening at http://127.0.0.1:' + PORT))
