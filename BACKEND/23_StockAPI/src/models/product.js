2ab86c3f7e54a3b61ad45f9d759676b8c0a069f2
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-04 10:37:32 +0100

    "Added product controller, model, and routes for CRUD operations"

--- BACKEND/23_StockAPI/src/models/product.js
+++ BACKEND/23_StockAPI/src/models/product.js
@@ -1,6 +1,41 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Product Model:
+
+const ProductSchema = new mongoose.Schema(
+  {
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    collection: "products",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Product", ProductSchema);
