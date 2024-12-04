f45e60bf04cb41d68fd312f6ac9744e43aac8e83
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-04 12:30:42 +0100

    "Refactored Swagger API configuration and updated User model schema with additional validation and encryption"

--- BACKEND/23_StockAPI/src/models/user.js
+++ BACKEND/23_StockAPI/src/models/user.js
@@ -3,14 +3,28 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
-/* ------------------------------------------------------- */
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+/* ----------------------------------------------------- *
+{
+  "username": "admin",
+  "password": "aA?123456",
+  "email": "admin@site.com",
+  "firstName": "admin",
+  "lastName": "admin",
+  "isActive": true,
+  "isStaff": true,
+  "isAdmin": true
+}
+/* ----------------------------------------------------- */
+
 const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
       trim: true,
       required: true,
-      uniuqe: true,
+      unique: true,
       index: true,
     },
 
@@ -24,7 +38,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      uniuqe: true,
+      unique: true,
       index: true,
     },
 
@@ -55,7 +69,50 @@ const UserSchema = new mongoose.Schema(
       default: false,
     },
   },
-  { collection: "users", timestamps: true }
+  {
+    collection: "users",
+    timestamps: true,
+  }
 );
+
+/* ----------------------------------------------------- */
+
+UserSchema.pre(["save", "updateOne"], function (next) {
+  // run before saving db
+  const data = this?._update ?? this;
+
+  // Email Control
+  const isEmailValidated = data.email
+    ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
+    : true;
+
+  if (!isEmailValidated) {
+    next(new Error("Email is not validated"));
+  }
+
+  const isPasswordValidated = data.password
+    ? /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/.test(
+        data.password
+      )
+    : true;
+
+  if (!isPasswordValidated) {
+    next(
+      new Error(
+        "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one special character"
+      )
+    );
+  }
+
+  if (data.password) {
+    if (this?._update) {
+      this._update.password = passwordEncrypt(data.password);
+    }
+  } else {
+    this.password = passwordEncrypt(data.password);
+  }
+
+  next();
+});
 
 module.exports = mongoose.model("User", UserSchema);
