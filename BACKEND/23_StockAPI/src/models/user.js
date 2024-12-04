cb75df57a4da28cb8e342e86a6af3dde6391feb3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-04 09:26:10 +0100

    "Added UserSchema to user.js model with various fields and exported mongoose model"

--- BACKEND/23_StockAPI/src/models/user.js
+++ BACKEND/23_StockAPI/src/models/user.js
@@ -1,6 +1,61 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      uniuqe: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      uniuqe: true,
+      index: true,
+    },
+
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
