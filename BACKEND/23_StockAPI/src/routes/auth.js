69fb44bd50a2a8601088c9ec3a9a64f2af946261
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-05 10:40:14 +0100

    "Added auth routes"

--- BACKEND/23_StockAPI/src/routes/auth.js
+++ BACKEND/23_StockAPI/src/routes/auth.js
@@ -1,6 +1,17 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
+
+const auth = require("../controllers/auth");
+
+// URL: /auth
+
+router.post("/login", auth.login);
+router.post("/refresh", auth.refresh);
+router.get("/logout", auth.logout);
+
+/* ------------------------------------------------------- */
+module.exports = router;
