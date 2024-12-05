a9f3ab78faf8a549b599660f9b4ac6859b411cd5
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-05 10:32:09 +0100

    "Added jsonwebtoken dependency and implemented authentication middleware with JWT and token-based authentication"

--- BACKEND/23_StockAPI/src/controllers/auth.js
+++ BACKEND/23_StockAPI/src/controllers/auth.js
@@ -3,6 +3,11 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const Token = require("../models/token");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
 module.exports = {
   login: async (req, res) => {
     /*
--- BACKEND/23_StockAPI/src/middlewares/authentication.js
+++ BACKEND/23_StockAPI/src/middlewares/authentication.js
@@ -1,10 +1,34 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 // app.use(authentication):
 
-module.exports = async (req, res, next) => {
+const jwt = require("jsonwebtoken");
+const Token = require("../models/token");
 
-    next()
-}
+module.exports = async (req, res, next) => {
+  const auth = req.headers?.authorization || null;
+  const tokenKey = auth ? auth.split(" ") : null;
+
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+      // SimpleToken
+
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      req.user = tokenData ? tokenData.userId : undefined;
+    } else if (tokenKey[0] == "Bearer") {
+      // JWT
+
+      jwt.verify(
+        tokenKey[1],
+        process.env.ACCESS_KEY,
+        (err, userData) => (req.user = userData)
+      );
+    }
+  }
+
+  next();
+};
