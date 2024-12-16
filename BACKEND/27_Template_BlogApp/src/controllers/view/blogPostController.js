9600bf69e833036bc01545109d0e7d6d5daa1c15
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-16 12:27:59 +0100

    `Added pagination URL parameter handling and updated blog post controller to render pagination links with the parameter`

--- BACKEND/27_Template_BlogApp/src/controllers/view/blogPostController.js
+++ BACKEND/27_Template_BlogApp/src/controllers/view/blogPostController.js
@@ -5,6 +5,7 @@
 
 const BlogPost = require("../../models/blogPostModel");
 const BlogCategory = require("../../models/blogCategoryModel");
+const removeQueryParam = require("../../helpers/removeQueryParam");
 
 // ------------------------------------------
 // BlogPost
@@ -12,67 +13,87 @@ const BlogCategory = require("../../models/blogCategoryModel");
 
 module.exports = {
   list: async (req, res) => {
-
     // list all published posts
-    const data = await res.getModelList(BlogPost, { published: true }, "blogCategoryId");
+    const data = await res.getModelList(
+      BlogPost,
+      { published: true },
+      "blogCategoryId"
+    );
     // list all categories
-    const categories = await BlogCategory.find()
+    const categories = await BlogCategory.find();
     // List recent 3 posts
-    const recentPosts = await BlogPost.find({ published: true }).sort({ createdAt: "desc" }).limit(3)
+    const recentPosts = await BlogPost.find({ published: true })
+      .sort({ createdAt: "desc" })
+      .limit(3);
     // Get page details
-    const details = await res.getModelListDetails(BlogPost, { published: true })
+    const details = await res.getModelListDetails(BlogPost, {
+      published: true,
+    });
 
-    res.render('index', { categories, posts: data, recentPosts, details })
+    // req.originalUrl
+    // console.log(req.originalUrl);
+    let pageUrl = "";
+    const queryString = req.originalUrl.split("?")[1];
+
+    if (queryString) {
+      pageUrl = removeQueryParam(queryString, "page");
+    }
+
+    pageUrl = pageUrl ? "&" + pageUrl : "";
+
+    res.render("index", {
+      categories,
+      posts: data,
+      recentPosts,
+      details,
+      pageUrl,
+    });
   },
 
   create: async (req, res) => {
-
-
-    if (req.method == 'POST') {
-      req.body.userId = req.session?.user.id
+    if (req.method == "POST") {
+      req.body.userId = req.session?.user.id;
       const data = await BlogPost.create(req.body);
-      res.redirect('/blog/post')
-
+      res.redirect("/blog/post");
     } else {
-
       const categories = await BlogCategory.find();
-      res.render('postForm', { post: null, categories })
+      res.render("postForm", { post: null, categories });
     }
-
   },
 
   read: async (req, res) => {
     // req.params.postId
     // const data = await BlogPost.findById(req.params.postId)
-    const data = await BlogPost.findOne({ _id: req.params.postId }).populate("blogCategoryId"); // get Primary Data
+    const data = await BlogPost.findOne({ _id: req.params.postId }).populate(
+      "blogCategoryId"
+    ); // get Primary Data
 
-    res.render('postRead', { post: data })
-
+    res.render("postRead", { post: data });
   },
 
   update: async (req, res) => {
     // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
 
-    if (req.method == 'POST') {
+    if (req.method == "POST") {
+      const data = await BlogPost.updateOne(
+        { _id: req.params.postId },
+        req.body,
+        { runValidators: true }
+      );
 
-      const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body, { runValidators: true });
-
-      res.redirect('/blog/post')
-
+      res.redirect("/blog/post");
     } else {
-
-      const post = await BlogPost.findOne({ _id: req.params.postId }).populate("blogCategoryId");
+      const post = await BlogPost.findOne({ _id: req.params.postId }).populate(
+        "blogCategoryId"
+      );
       const categories = await BlogCategory.find();
 
-      res.render('postForm', { post, categories })
+      res.render("postForm", { post, categories });
     }
-
-
   },
 
   //Todo -> complete this controller
   delete: async (req, res) => {
     const data = await BlogPost.deleteOne({ _id: req.params.postId });
-
   },
 };
